91168ba4609b4212f8db192354ba0d7e10334add
Nick Krambousanos <nkrambo@gmail.com>
2016-07-12 19:47:45 -0700

    Add in 1.4

---
+++ cci/chapter_1/1.4.py
@@ -0,0 +1,16 @@
+
+""" Palindrome permutation: Given a string, write a function to
+    check if it is a permutation of a palindrome. A palindrome is a word
+    or phrase that is the same forwards as it is backwards. A permutation is a
+    rearrangment of letters. The palindrome does not need to be limited to just
+    dictionary words."""
+
+# input - 'Tact Coa'
+# output - True (Permutations: 'taco cat', 'atco cta' etc...)
+
+def palindrome_perm(string):
+
+    # lowercase and trim?
+
+# test cases
+print(palindrome_perm('Tact Coa'))
