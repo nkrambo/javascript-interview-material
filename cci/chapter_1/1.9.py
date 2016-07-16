fa925f589e317dfc30a758d840e2a9b6f9f595d6
Nick Krambousanos <nkrambo@gmail.com>
2016-07-16 13:17:33 -0700

    Stub out chapter 1

---
+++ cci/chapter_1/1.9.py
@@ -0,0 +1,8 @@
+
+""" String Rotation: Assume you have a method isSubstring which
+    checks if one word is a substring of another. Given 2 strings,
+    s1 and s2, write code to check if s2 is a rotation of s1 using
+    only one call to isSubstring (e.g, 'waterbottle' is a rotation
+    of 'erbottlewat') """
+
+""" Notes: """
