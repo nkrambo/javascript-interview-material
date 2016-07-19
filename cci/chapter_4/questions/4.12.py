b361d7ed15e991d45410e4bcc4c123a1f5ada306
Nick Krambousanos <nkrambo@gmail.com>
2016-07-19 13:04:15 -0700

    Chap 4

---
+++ cci/chapter_4/questions/4.12.py
@@ -0,0 +1,8 @@
+
+""" Paths with Sum: You are given a binary search tree in which each node
+    contains an integer value (which might be positive or negative). Design
+    an algorithm to count the number of paths that sum to a given value.
+    The path does not need to start or end at the root or a leaf, but it must
+    go downwards (traveling only from parent nodes to child nodes)."""
+
+""" Notes: """
