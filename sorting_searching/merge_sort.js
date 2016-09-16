1c5147663c014a4747faf33b9703f6ff37ed4fd8
Nick Krambousanos <nkrambo@gmail.com>
2016-09-16 14:00:22 -0700

    Added merge sort

---
+++ sorting_searching/merge_sort.js
@@ -0,0 +1,39 @@
+
+// merge sort
+// @param {array integers} array - Takes an array of unsorted integers
+// @return {array} Returns a new sorted, ascending array
+
+function mergeSort(array) {
+
+  // type check
+  if (!Array.isArray(array)) {
+    new Error('mergeSort: Expects a single parameter of [array] type.');
+  }
+
+  // base
+  if (array.length <= 1) return array;
+
+  // grab the middle element, lean right if even
+  const middle = Math.floor(array.length / 2);
+
+  const left = mergeSort(array.slice(0, middle));
+  const right = mergeSort(array.slice(middle));
+
+  return merge(left, right);
+}
+
+function merge (left, right) {
+  const result = [];
+
+  while (left.length > 0 && right.length > 0) {
+    result.push(left[0] < right[0] ? left.shift() : right.shift());
+  }
+
+  return result.concat(left.length ? left : right);
+}
+
+const test = [1, 2, 9, 3, 2, 5, 14, 0];
+console.log(mergeSort(test)); // [0, 1, 2, 2, 3, 5, 9, 14]
+
+
+
