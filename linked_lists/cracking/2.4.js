b51cb9eedcca4086be16d5ab4de84a7abbe9faa6
Nick Krambousanos <nkrambo@gmail.com>
2016-09-19 14:43:51 -0700

    2.5

--- linked_lists/cracking/2.4.js
+++ linked_lists/cracking/2.4.js
@@ -1,3 +1,41 @@
 
 // Partition
+// @param {object} list - The LinkedList object to partitionList
+// @param {integer} pivot - The value to partition the list at
 
+function partitionList(list, pivot) {
+
+  // type check
+  if (!(list instanceof LinkedList)) {
+    new Error('partitionList: Expects first parameter of type [object LinkedList]');
+  }
+
+  // edge cases
+  if (list.length < 2) {
+    new Error('partitionList: Cannnot partition list of length 2 nodes or less.');
+  }
+
+  let head = list.head;
+  let tail = list.head;
+
+  let current = list.head;
+
+  while (current !== null) {
+
+    // insert node at head
+    if (current.value < pivot) {
+      current.next = head;
+      head = current;
+
+    // insert at tail
+    } else {
+      tail.next = current;
+      tail = current;
+    }
+
+    current = current.next;
+  }
+
+  // tail.next = null;
+  // return head;
+}
--- linked_lists/cracking/2.5.js
+++ linked_lists/cracking/2.5.js
@@ -1,3 +1,47 @@
 
 // Sum Lists
 
+function sumLists(list1, list2) {
+
+  const results = new LinkedList();
+
+  let node1 = list1.head;
+  let node2 = list2.head;
+  let carry = 0;
+  let sum;
+
+  // add up matched nodes
+  while (node1 && node2) {
+    sum = node1.value + node2.value + carry;
+    if (sum >= 10) {
+      carry = 1;
+      sum -= 10;
+    } else {
+      carry = 0;
+    }
+    results.add(sum, results.length);
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+
+  // get any remaining values
+  let node = node1 || node2;
+
+  while (node) {
+    sum = node.value + carry;
+    if (sum >= 10) {
+      carry = 1;
+      sum -= 10;
+    } else {
+      carry = 0;
+    }
+    results.add(sum, results.length);
+    node = node.next;
+  }
+
+  if (carry > 0) results.add(carry, results.length);
+
+  return results;
+}
+
+
