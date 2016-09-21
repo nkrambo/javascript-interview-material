fb06ac849b38baf31018d6183a5aad17a47d1d50
Nick Krambousanos <nkrambo@gmail.com>
2016-09-21 14:44:46 -0700

    4.7

---
+++ trees_graphs/_questions/cracking/4.7.js
@@ -0,0 +1,15 @@
+
+// Build Order
+
+// You are given a list of projects and a list of dependencies (which is a
+// list of pairs of projects, where the first project is dependent on the second project).
+// All of a project's dependencies must be built before the project is. Find a build order that
+// will allow the projects to be built. If there is no valid build order, return an error.
+
+// @param {array} projects - Array of projects
+// @param {array arrays} deps - A matrix of project dependencies
+// @return {array} Returns a new array with the project build order, or an error if no valid order exists.
+
+function buildOrder(projects, deps) {
+
+}
