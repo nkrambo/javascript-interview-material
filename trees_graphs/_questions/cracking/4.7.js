447a71a19749dc3ead179ae2fd4f52bd43587fb8
Nick Krambousanos <nkrambo@gmail.com>
2016-09-21 18:44:09 -0700

    4.7

--- trees_graphs/_questions/cracking/4.7.js
+++ trees_graphs/_questions/cracking/4.7.js
@@ -10,6 +10,52 @@
 // @param {array arrays} deps - A matrix of project dependencies
 // @return {array} Returns a new array with the project build order, or an error if no valid order exists.
 
-function buildOrder(projects, deps) {
+// Using a topological approach
+// Page 623
 
+function buildOrder(projects, deps) {
+  const finished = [];
+  const discovered = new Set();
+  const path = new Set();
+
+  // create graph
+  const graph = {};
+  projects.forEach((project) => {
+    graph[project] = [];
+  });
+
+  // add in deps
+  dependencies.forEach((edge) => {
+   graph[edge[1]].push(edge[0]);
+  });
+
+  // run topological sort
+  projects.forEach((project) => {
+    topologicalSort(graph, discovered, finished, path, project);
+  });
+
+  return finished.reverse();
 }
+
+function topologicalSort(graph, discovered, finished, path, project) {
+  if (discovered.has(project)) return;
+
+  discovered.add(project);
+  path.add(project);
+
+  for (let neighbour of graph[project]) {
+    if (path.has(neighbour)) throw new Error('dependencies are cyclic');
+    topologicalSort(graph, discovered, finished, path, neighbour);
+  }
+
+  path.delete(project);
+  finished.push(project);
+}
+
+const projects = ['a', 'b', 'f', 'd', 'g', 'e'];
+const deps = [
+  ['a', 'b'],
+  ['f', 'b']
+];
+
+console.log(buildOrder(projects, deps));
