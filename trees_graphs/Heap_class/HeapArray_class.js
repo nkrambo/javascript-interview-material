9a99cde21a587c9a954bd188e68e9aaa7a901131
Nick Krambousanos <nkrambo@gmail.com>
2016-09-23 18:21:21 -0700

    binary heap

--- trees_graphs/Heap_class/HeapArray_class.js
+++ trees_graphs/Heap_class/HeapArray_class.js
@@ -2,8 +2,9 @@
 // http://eloquentjavascript.net/1st_edition/appendix2.html
 
 class BinaryHeap {
-  constructor() {
+  constructor(scoreFunction) {
     this.items = [];
+    this.scoreFunction = scoreFunction;
   }
 
   push(value) {
@@ -26,7 +27,27 @@ class BinaryHeap {
     return root;
   }
 
-  remove() {}
+  remove(value) {
+
+    // iterate array to find node
+    for (let i = 0; i < this.items.length; i++) {
+
+      if (this.items[i] === value) {
+
+        // grab the last to fill the hole left from removal
+        const last = this.items.pop();
+
+        // If the element we popped was the one to remove, we're done.
+        if (i === this.items.length - 1) break;
+
+        // Otherwise, replace the removed with last and bubble or sink as appropriate
+        this.items[i] = last;
+        this.bubbleUp(i);
+        this.sinkDown(i);
+        break;
+      }
+    }
+  }
 
   size() {
     return this.items.length;
@@ -54,133 +75,45 @@ class BinaryHeap {
   }
 
   sink(node) {
-    // Look up the target element and its score.
-    const node = this.items[node];
-    const elemScore = this.scoreFunction(node);
+    // Look up node and its score.
+    const item = this.items[node];
+    const itemScore = this.scoreFunction(node);
 
     while(true) {
-
       // Compute the indices of children.
-      const left = (n + 1) * 2;
+      const left = (node + 1) * 2;
       const right = left - 1;
 
       // store the new position of the element, if any.
       let swap = null;
 
-      // If the left child exists
+      // If left exists, grab score and compare it
       if (left < this.items.length) {
+        const leftScore = this.scoreFunction(this.items[left]);
 
-        // Look it up and compute its score.
-        var child1 = this.content[child1N],
-        child1Score = this.scoreFunction(child1);
-
-        // If the score is less than our element's, we need to swap.
-        if (child1Score < elemScore)
-          swap = child1N;
+        // If score is less than item's, swap them.
+        if (leftScore < itemScore) {
+          swap = left;
+        }
       }
 
       // Do the same for the right.
-      if (child2N < this.items.length) {
-        var child2 = this.content[child2N],
-        child2Score = this.scoreFunction(child2);
-        if (child2Score < (swap == null ? elemScore : child1Score))
-          swap = child2N;
+      if (right < this.items.length) {
+        const rightScore = this.scoreFunction(this.items[right]);
+        if (rightScore < (swap == null ? itemScore : leftScore)) {
+          swap = right;
+        }
       }
 
       // No need to swap further, we are done.
       if (swap == null) break;
 
       // Otherwise, swap and continue.
-      this.content[n] = this.content[swap];
-      this.content[swap] = element;
-      n = swap;
+      this.items[node] = this.items[swap];
+      this.items[swap] = item;
+      node = swap;
     }
   }
 }
 
 export default BinaryHeap;
-
-BinaryHeap.prototype = {
-
-  remove: function(node) {
-    var length = this.content.length;
-    // To remove a value, we must search through the array to find
-    // it.
-    for (var i = 0; i < length; i++) {
-      if (this.content[i] != node) continue;
-      // When it is found, the process seen in 'pop' is repeated
-      // to fill up the hole.
-      var end = this.content.pop();
-      // If the element we popped was the one we needed to remove,
-      // we're done.
-      if (i == length - 1) break;
-      // Otherwise, we replace the removed element with the popped
-      // one, and allow it to float up or sink down as appropriate.
-      this.content[i] = end;
-      this.bubbleUp(i);
-      this.sinkDown(i);
-      break;
-    }
-  },
-
-
-  bubbleUp: function(n) {
-    // Fetch the element that has to be moved.
-    var element = this.content[n], score = this.scoreFunction(element);
-    // When at 0, an element can not go up any further.
-    while (n > 0) {
-      // Compute the parent element's index, and fetch it.
-      var parentN = Math.floor((n + 1) / 2) - 1,
-      parent = this.content[parentN];
-      // If the parent has a lesser score, things are in order and we
-      // are done.
-      if (score >= this.scoreFunction(parent))
-        break;
-
-      // Otherwise, swap the parent with the current element and
-      // continue.
-      this.content[parentN] = element;
-      this.content[n] = parent;
-      n = parentN;
-    }
-  },
-
-  sinkDown: function(n) {
-    // Look up the target element and its score.
-    var length = this.content.length,
-    element = this.content[n],
-    elemScore = this.scoreFunction(element);
-
-    while(true) {
-      // Compute the indices of the child elements.
-      var child2N = (n + 1) * 2, child1N = child2N - 1;
-      // This is used to store the new position of the element,
-      // if any.
-      var swap = null;
-      // If the first child exists (is inside the array)...
-      if (child1N < length) {
-        // Look it up and compute its score.
-        var child1 = this.content[child1N],
-        child1Score = this.scoreFunction(child1);
-        // If the score is less than our element's, we need to swap.
-        if (child1Score < elemScore)
-          swap = child1N;
-      }
-      // Do the same checks for the other child.
-      if (child2N < length) {
-        var child2 = this.content[child2N],
-        child2Score = this.scoreFunction(child2);
-        if (child2Score < (swap == null ? elemScore : child1Score))
-          swap = child2N;
-      }
-
-      // No need to swap further, we are done.
-      if (swap == null) break;
-
-      // Otherwise, swap and continue.
-      this.content[n] = this.content[swap];
-      this.content[swap] = element;
-      n = swap;
-    }
-  }
-};
