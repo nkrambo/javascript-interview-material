562ebce55198641a3d4c391e99af3358f4272089
Nick Krambousanos <nkrambo@gmail.com>
2016-10-03 12:56:59 -0700

    check permutation updates, testing

--- arrays_strings/questions/check_permutation/check_permutation.js
+++ arrays_strings/questions/check_permutation/check_permutation.js
@@ -1,35 +1,64 @@
+
 /**
- * Keep track of characters counts with a Map data structure, fail when
- * str2 has a character different to str2 or if any characters are left over
- * at the end.
- *
- * N = |str1| && M = |str2|
- * Time: O(N + M)
- * Additional space: O(N)
- *
- * @param  {string[]} str2 First string, passed in as a character array
- * @param  {string[]} str1 Second string, passed in as a character array
- * @return {boolean}       True if first and second strings are permutations otherwise false
- */
+* Check Permutation
+*
+* Given two strings, write a method to check if one is a permutation of
+* the other.
+*
+*/
 
-function isPermutationMap(str1, str2) {
+/**
+* isPermutation()
+*
+* Solution:
+* As a quick first step to solving this would be to compare the lengths of each
+* string. If they are different, then they cannot possibly be permutations and we
+* return false.
+*
+* Otherwise, we can use a Map() to keep track of the characters, and their count,
+* as we iterate over str1.
+*
+* We can then iterate over str2 and decrement, or remove, characters from the map
+* as we come across them. If the two strings are permutations of each other, there
+* should be an exact same count of characters in both. Therefore, there should be no
+* leftover characters in the map.
+*
+* We can return false if there are characters left in the map, or if we come across a
+* a completely new character.
+*
+* Time: O(n + m)
+* Space: O(n)
+* Where n is the length of str1, m is the length of str2
+*
+* @param  {string} str2 First string of (n) length
+* @param  {string} str1 Second string of (n) length
+* @return {boolean} Returns true if str1 and str2 are permutations of each other, otherwise false.
+*/
 
+function isPermutation(str1, str2) {
+
+  // type check
   if (typeof str1 !== 'string' || typeof str2 !== 'string') {
-    throw new Error('Must pass in two strings');
+    throw new TypeError('isPermutation: Expects 2 arguments of [string] type.');
   }
 
+  // length check, short-circuit
   if (str1.length !== str2.length) return false;
 
-  let chars = new Map();
-
-  for (let i = 0; i < str1.length; ++i) {
+  // count characters in str1
+  const chars = new Map();
+  for (let i = 0; i < str1.length; i += 1) {
     chars.set(str1[i], chars.get(str1[i]) + 1 || 1); // increment or set to 1
   }
 
-  for (let i = 0; i < str2.length; ++i) {
+  // check str2 against str1 map
+  for (let i = 0; i < str2.length; i += 1) {
     let count = chars.get(str2[i]);
+
+    // if character doesn't exist
     if (!count) return false;
 
+    // remove if last count, or decrement
     if (count === 1) {
       chars.delete(str2[i]);
     } else {
@@ -41,21 +70,35 @@ function isPermutationMap(str1, str2) {
 }
 
 /**
- * Sort both strings and check if they are equal afterwards. Permutations will
- * be identical sorted strings.
- *
- * N = |str1| && M = |str2|
- * Time: O(N lg N + M lg M)
- * Additional space: O(1) if able to modify original strings, O(N + M) otherwise
- *
- * @param  {string[]} str2 First string, passed in as a character array
- * @param  {string[]} str1 Second string, passed in as a character array
- * @return {boolean}       True if first and second strings are permutations otherwise false
- */
+* isPermutationSort()
+*
+* Solution:
+* This is a simple solution that compares the strings by sorting them.
+* Permutations will be always identical sorted strings. We have to chain a
+* few methods together to acheive this, which places our runtime higher than
+* the alternative approach.
+*
+* Time: O((n log n) + (m log m))
+* Space: O(1)
+* Where n is the length of str1, m is the length of str2
+*
+* @param  {string} str2 First string of (n) length
+* @param  {string} str1 Second string of (n) length
+* @return {boolean} Returns true if str1 and str2 are permutations of each other, otherwise false.
+*/
 
-function isPermutationSort(string1, string2) {
-  if (string1.length !== string2.length) return false;
-  return string1.split('').sort().join() === string2.split('').sort().join();
+function isPermutationSort(str1, str2) {
+
+  // type check
+  if (typeof str1 !== 'string' || typeof str2 !== 'string') {
+    throw new TypeError('isPermutation: Expects 2 arguments of [string] type.');
+  }
+
+  // length check, short-circuit
+  if (str1.length !== str2.length) return false;
+
+  // compare the sorted strings
+  return str1.split('').sort().join() === str2.split('').sort().join();
 }
 
-
+export { isPermutation, isPermutationSort };
