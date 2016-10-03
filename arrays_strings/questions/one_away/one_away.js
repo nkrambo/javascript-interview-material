6c3a5a1df0546a7f4d0e1c3f3166e14eb9a72454
Nick Krambousanos <nkrambo@gmail.com>
2016-10-03 16:03:56 -0700

    clean up oneAway, tested

--- arrays_strings/questions/one_away/one_away.js
+++ arrays_strings/questions/one_away/one_away.js
@@ -1,34 +1,69 @@
 
-// one away
-// @param {string} string1 String of n length
-// @param {string} string2 String of n length
-// @return {boolean} Returns true if the difference between the two strings in 1 or less edits
+/**
+* One Away
+*
+* There are three types of edits that can be performed on strings: insert a
+* character, remove a character, or replace a character. Given two strings,
+* write a function to check if they are one edit (or zero edits) away.
+*
+* Example:
+* pale, ple -> true
+* pales, pale -> true
+* pale, bale -> true
+* pale, bae -> false
+*/
 
-function oneAway(string1, string2) {
+/**
+* oneAway()
+*
+* Solution:
+* We can possibly do a fast return by comparing the lengths of the twi strings.
+* If the lengths differ by more than 1, we know that they must differ by more than 1 edit.
+*
+* Otherwise we can use a map to track the type and count of characters in str1.
+* We then also interate over str2 and compare the occurence of characters, removing and
+* decremting as needed.
+*
+* If the strings are only 1 edit (or less) apart, then the size of the map should be
+* less than or equal to 1, representing the 1 edit allowed.
+*
+* Time: O(n)
+* Space: O(n)
+* Where n is the length of the string input.
+*
+* @param {string} str1 String of (n) length
+* @param {string} str2 String of (n) length
+* @return {boolean} Returns true if the difference between the two strings in 1 or less edits,
+* otherwise returns false;
+*/
+
+function oneAway(str1, str2) {
 
   // type check
-  if (typeof string1 !== 'string' || typeof string2 !== 'string') {
-    throw new Error('function oneAway(): Requires 2 string type parameters');
+  if (typeof str1 !== 'string' || typeof str2 !== 'string') {
+    throw new TypeError('function oneAway(): Expects 2 arguments of [string] type.');
   }
 
-  // if lengths differ by more than 1 then can't be true
-  if (Math.abs(string1.length - string2.length) > 1) return false;
+  // if lengths differ by more than 1, must be false
+  if (Math.abs(str1.length - str2.length) > 1) return false;
 
   const chars = new Map();
-  for (let i = 0; i < string1.length; i++) {
-    chars.set(string1[i], chars.get(string1[i]) + 1 || 1); // increment by or set to 1
+  for (let i = 0; i < str1.length; i += 1) {
+    chars.set(str1[i], chars.get(str1[i]) + 1 || 1); // increment by or set to 1
   }
 
-  for (let i = 0; i < string2.length; i++) {
-    let count = chars.get(string2[i]);
+  for (let i = 0; i < str2.length; i += 1) {
+    let count = chars.get(str2[i]);
     if (count) {
       if (count === 1) {
-        chars.delete(string2[i]);
+        chars.delete(str2[i]);
       } else {
-       chars.set(string2[i], count - 1);
+       chars.set(str2[i], count - 1);
       }
     }
   }
 
   return chars.size <= 1;
 }
+
+export default oneAway;
---
+++ arrays_strings/questions/one_away/test/one_away.spec.js
@@ -0,0 +1,20 @@
+
+import { expect } from 'chai';
+import oneAway from '../one_away';
+
+describe('oneAway()', () => {
+  it('should return true for edits 1 or less', () => {
+    expect(oneAway('pale', 'ple')).to.be.true;
+    expect(oneAway('pales', 'pale')).to.be.true;
+    expect(oneAway('pale', 'bale')).to.be.true;
+  });
+
+  it('should return false for edits more than 1', () => {
+    expect(oneAway('pale', 'bae')).to.be.false;
+  });
+
+  it('should throw a type error', () => {
+    expect(function(){ oneAway (5, 5); }).to.throw (TypeError);
+  });
+
+});
