f4a44f0b92f313e40f1f90498b2f0237e7fafeb4
Nick Krambousanos <nkrambo@gmail.com>
2016-10-09 13:26:33 -0700

    add strings stub

---
+++ strings/questions/__add_strings/add_strings.js
@@ -0,0 +1,33 @@
+
+/**
+* Add Strings
+*
+* Given two non-negative numbers num1 and num2 represented as string, return
+* the sum of num1 and num2.
+*
+* Notes:
+*
+* 1. The length of both num1 and num2 is < 5100.
+* 2. Both num1 and num2 contains only digits 0-9.
+* 3. Both num1 and num2 does not contain any leading zero.
+* 4. You must not use any built-in library or convert the inputs to integer directly.
+*/
+
+/**
+* addStrings()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} num1 string representation of number < 5100, containing only 0-9
+* @param {string} num2 string representation of number < 5100, containing only 0-9
+* @return {number} returns sum of num1 and num2 as integer
+*/
+
+function addStrings() {
+
+}
+
+export default addStrings;
---
+++ strings/questions/__add_strings/test/add_strings.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import addStrings from '../add_strings';
+
+describe('addStrings()', () => {
+  // it('should', () => {
+  //
+  // });
+});
