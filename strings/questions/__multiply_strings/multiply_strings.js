a588bc6a8f6829e57b6ca19a068decdd37ba6d10
Nick Krambousanos <nkrambo@gmail.com>
2016-10-09 16:39:27 -0700

    stub multiply strings

---
+++ strings/questions/__multiply_strings/multiply_strings.js
@@ -0,0 +1,32 @@
+
+/**
+* Multiply Strings
+*
+* Given two numbers represented as strings, return multiplication of the
+* numbers as a string.
+*
+* Notes:
+* 1. The numbers can be arbitrarily large and are non-negative.
+* 2. Converting the input string to integer is NOT allowed.
+* 3. You should NOT use any librarys.
+*/
+
+/**
+* multiplyStrings()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} str1 integer represented as string
+* @param {string} str2 integer represented as string
+* @return {number} returns the multiplication of str1 and str2
+*/
+
+
+function multiplyStrings(str1, str2) {
+
+}
+
+export default multiplyStrings;
