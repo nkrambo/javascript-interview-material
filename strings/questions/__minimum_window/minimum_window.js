1d4ac55fb4281438e7014bdf0f1c6762f19b0061
Nick Krambousanos <nkrambo@gmail.com>
2016-10-09 16:33:20 -0700

    stub minimum window

---
+++ strings/questions/__minimum_window/minimum_window.js
@@ -0,0 +1,37 @@
+
+/**
+* Minimum Window String
+*
+* Given a string str1 and a string str2, find the minimum window in str1 which
+* will contain all the characters in str2 in complexity O(n).
+*
+* Example:
+* Input: 'ADOBECODEBANC', 'ABC'
+* Output: 'BANC'
+*
+* Notes:
+* If there is no such window in str1 that covers all characters in str2, return
+* an empty string ''.
+*
+* If there are multiple such windows, you are guaranteed that there will always
+* be only one unique minimum window in str1.
+*/
+
+/**
+* minimumWindow()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} str1 base string
+* @param {string} str2 string to find in base string
+* @return {string} returns the minimum window in str1, containing str2
+*/
+
+function minimumWindow(str1, str2) {
+
+}
+
+export default minimumWindow;
---
+++ strings/questions/__minimum_window/test/minimum_window.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import minimumWindow from '../minimum_window';
+
+describe('minimumWindow()', () => {
+  // it('should', () => {
+  //
+  // });
+});
