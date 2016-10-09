48da06d45b95124e52190cf82bf716020ea1d87c
Nick Krambousanos <nkrambo@gmail.com>
2016-10-09 13:14:12 -0700

    stub

---
+++ strings/questions/__isomorphic_strings/isomorphic_strings.js
@@ -0,0 +1,45 @@
+
+/**
+* Isomorphic Strings
+*
+* Given two strings str1 and str2, determine if they are isomorphic.
+*
+* Two strings are isomorphic if the characters in str1 can be replaced to get str2.
+*
+* All occurrences of a character must be replaced with another character while
+* preserving the order of characters. No two characters may map to the same
+* character but a character may map to itself.
+*
+* Example:
+* Input: 'egg', 'add'
+* Output: true
+*
+* Example:
+* Input: 'foo', 'bar'
+* Output: false
+*
+* Example:
+* Input: 'paper', 'title'
+* Output: true
+*
+* You may assume both str1 and str2 have the same length.
+*/
+
+/**
+* isIsomorphic()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} str1 string to check
+* @param {string} str2 string to check
+* @return {boolean} returns true if the strings are isomorphic, otherwise false
+*/
+
+function isIsomorphic() {
+
+}
+
+export default isIsomorphic;
---
+++ strings/questions/__isomorphic_strings/test/isomorphic_strings.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import isIsomorphic from '../isomorphic_strings';
+
+describe('isIsomorphic()', () => {
+  // it('should', () => {
+  //
+  // });
+});
