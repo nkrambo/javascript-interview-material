d15f79db742b10fdfe871fd58156bd4d2edd960d
Nick Krambousanos <nkrambo@gmail.com>
2016-10-10 15:01:18 -0700

    ransom note

--- strings/questions/__ransom_note/ransom_note.js
+++ strings/questions/__ransom_note/ransom_note.js
@@ -28,6 +28,42 @@
 *
 * Solution:
 *
+* We can approach this one by iterating over the ransom note and storing the type
+* and count of each character that appears into a Map, ignoring spaces.
+*
+* We then iterate over magazine and decrement our ransom note character Map as we
+* come across new characters, again ignoring spaces.
+*
+* As soon as our Map hits a 0 size, we know that all characters are accounted for,
+* and we can return true, otherwise we'll return false by default.
+*
+* This approach works better than returning at the bottom of our function
+* if chars.size === 0. Imagine a short ransom note, say 250 characters, and a
+* very long magazine, say 20,000 characters. We only want to iterate the magazine
+* for as long as we have outstanding characters in the ransom note, no longer.
+*
+* Another couple of things.
+*
+* You might be tempted to short-circuit if the magazine is shorter than the ransom
+* note, but this will not always return a true false.
+*
+* For example:
+* ransomeNote = 'a b c'; // length = 5
+* magazine = 'abc'; // length = 3
+*
+* In this example, whitespace is account for in our magazine.length property.
+* If we had short-cicuited this would return false when, in fact, we can construct
+* a ransom note from the magazine.
+*
+* You could remove whitespace from the strings to work with them a little easier,
+* but there really isn't an efficient method to acheive this.
+*
+* For example:
+* ransomNote = ransomNote.replace(/\s/g, '');
+*
+* The replace method might or might not have a runtime of O(n), however, it's just as easy
+* to skip over spaces when we iterate.
+*
 * Time: O(1)
 * Space: O(1)
 *
@@ -37,35 +73,45 @@
 * magainze, otherwise false
 */
 
-function ransomNote(ransomeNote, magazine) {
+function ransomNote(ransomNote, magazine) {
 
+  // type check
+  if (typeof ransomNote !== 'string' || typeof magazine !== 'string') {
+    throw new TypeError('ransomNote: Expects 2 arguments of [string] type.');
+  }
+
+  const chars = new Map();
+  for (let i = 0; i < ransomNote.length; i += 1) {
+    let char = ransomNote[i];
+
+    // ignore spaces
+    if (char === ' ') continue;
+
+    // otherwise, count up characters
+    chars.set(char, chars.get(char) + 1 || 1); // increment by or add 1
+  }
+
+  // loop over magazine
+  for (let i = 0; i < magazine.length; i++) {
+    let char = magazine[i];
+    let count = chars.get(char);
+
+    // if character isn't in ransom, move on
+    // or if we come across a space
+    if (!count || char === ' ') continue;
+
+    // remove or decrement characters
+    if (count === 1) {
+      chars.delete(char);
+    } else {
+      chars.set(char, count - 1);
+    }
+
+    // only return true if all chars are accounted for
+    if (chars.size === 0) return true;
+  }
+
+  return false;
 }
-
-// const canConstruct = function(ransomNote, magazine) {
-//
-//   if (ransomNote.length > magazine.length) {
-//     return false;
-//   }
-//
-//   const magChars = [];
-//   for (let i = 0; i < 128; i++) {
-//     magChars[i] = 0;
-//   }
-//
-//   for (let char of magazine) {
-//     magChars[char.charCodeAt(0)] += 1;
-//   }
-//
-//   for (let char of ransomNote) {
-//
-//     if (magChars[char.charCodeAt(0)] === 0) {
-//       return false;
-//     } else {
-//       magChars[char.charCodeAt(0)] -= 1;
-//     }
-//   }
-//
-//   return true;
-// };
 
 export default ransomNote;
--- strings/questions/__ransom_note/test/ransom_note.spec.js
+++ strings/questions/__ransom_note/test/ransom_note.spec.js
@@ -3,7 +3,18 @@ import { expect } from 'chai';
 import ransomNote from '../ransom_note';
 
 describe('ransomNote()', () => {
-  // it('should', () => {
-  //
-  // });
+  it('should return false if magazine < ransom note, white space excluded', () => {
+    expect(ransomNote('muchlonger', 'short')).to.be.false;
+  });
+
+  it('should return false if ransom cannot be constructed from magazine', () => {
+    expect(ransomNote('a', 'b')).to.be.false;
+    expect(ransomNote('aa', 'ab')).to.be.false;
+  });
+
+  it('should return true if ransom can be constructed from magazine', () => {
+    expect(ransomNote('a', 'a')).to.be.true;
+    expect(ransomNote('abc', 'a b c')).to.be.true;
+    expect(ransomNote('money or else', 'more money, more problems.')).to.be.true;
+  });
 });
