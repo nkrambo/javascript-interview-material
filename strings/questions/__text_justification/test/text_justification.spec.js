66a0dbbc21a7813579fb29075b87ab4ed7a4d3c4
Nick Krambousanos <nkrambo@gmail.com>
2016-10-11 16:57:51 -0700

    text justification

---
+++ strings/questions/__text_justification/test/text_justification.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import justify from '../text_justification';
+
+describe('justify()', () => {
+  // it('should', () => {
+  //
+  // });
+});
---
+++ strings/questions/__text_justification/text_justification.js
@@ -0,0 +1,45 @@
+
+/**
+* Text Justification
+*
+* Given an array of words and a length L, format the text such that each line
+* has exactly L characters and is fully (left and right) justified.
+*
+* You should pack your words in a greedy approach; that is, pack as many words
+* as you can in each line. Pad extra spaces ' ' when necessary so that each line
+* has exactly L characters.
+*
+* Extra spaces between words should be distributed as evenly as possible. If the
+* number of spaces on a line do not divide evenly between words, the empty slots
+* on the left will be assigned more spaces than the slots on the right.
+*
+* For the last line of text, it should be left justified and no extra space is
+* inserted between words.
+*
+* Example:
+* Input: ['This', 'is', 'an', 'example', 'of', 'text', 'justification'.], 16
+* Output: ['This    is    an',
+*          'example  of text',
+*          'justification.  ']
+*
+* Note: Each word is guaranteed not to exceed len in length.
+*/
+
+/**
+* justify()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {array} words array of strings
+* @param {number} len length of lines
+* @return {array} returns an array of length justified strings
+*/
+
+function justify(words, len) {
+
+}
+
+export default justify;
