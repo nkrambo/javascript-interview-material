ec6688895cd0ed4b867b59bf8f96e980a4ae4316
Nick Krambousanos <nkrambo@gmail.com>
2016-10-11 16:01:32 -0700

    split array largest sum

---
+++ arrays/questions/__split_array_largest_sum/split_array_largest_sum.js
@@ -0,0 +1,34 @@
+
+/**
+* Split Arary Largest Sum
+*
+* Given an array which consists of non-negative integers and an integer m, you
+* can split the array into m non-empty continuous subarrays. Write an algorithm
+* to minimize the largest sum among these m subarrays.
+*
+* Example:
+* Input: [7, 2, 5, 10, 8], 2
+* Output: 18
+* There are four ways to split nums into two subarrays. The best way is to split
+* it into [7, 2, 5] and [10, 8], where the largest sum among the two subarrays
+* is only 18.
+*/
+
+/**
+* splitArray()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {array} nums array of positive integers ≤ 14,000
+* @param {number} m number of times to split array, 1 ≤ m ≤ length(nums)
+* @return {number} return the largest sum amoung m arrays
+*/
+
+function splitArray(nums, m) {
+  
+}
+
+export default splitArray;
---
+++ arrays/questions/__split_array_largest_sum/test/split_array_largest_sum.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import { splitArray } from '../split_array_largest_sum';
+
+describe('splitArray()', () => {
+  // it('should', () => {
+  //
+  // });
+});
