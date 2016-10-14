7e2fd7a9f5a5fccb7f8f31b8f260f83b701d3a97
Nick Krambousanos <nkrambo@gmail.com>
2016-10-14 15:38:15 -0700

    stolen drone

---
+++ arrays/questions/__stolen_breakfast_drone/stolen_breakfast_drone.js
@@ -0,0 +1,42 @@
+
+/**
+* Stolen Breakfast Drone
+*
+* Your company delivers breakfast via autonomous quadcopter drones. And something
+* mysterious has happened.
+*
+* Each breakfast delivery is assigned a unique ID, a positive integer. When one
+* of the company's 100 drones takes off with a delivery, the delivery's ID is added
+* to an array, deliveryIdConfirmations. When the drone comes back and lands, the
+* ID is again added to the same array.
+*
+* After breakfast this morning there were only 99 drones on the tarmac. One of
+* the drones never made it back from a delivery. We suspect a secret agent from
+* Amazon placed an order and stole one of our patented drones. To track them down,
+* we need to find their delivery ID.
+*
+* Given the array of IDs, which contains many duplicate integers and one unique
+* integer, find the unique integer.
+*
+* The IDs are not guaranteed to be sorted or sequential. Orders aren't always
+* fulfilled in the order they were received, and some deliveries get cancelled
+* before takeoff.
+*/
+
+/**
+* findUniqueDeliveryId()
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {array} deliveryIds array of ids
+* @return {number} returns a unique id
+*/
+
+function findUniqueDeliveryId(deliveryIds) {
+
+}
+
+export default findUniqueDeliveryId;
---
+++ arrays/questions/__stolen_breakfast_drone/test/stolen_breakfast_drone.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import findUniqueDeliveryId from '../stolen_breakfast_drone';
+
+describe('findUniqueDeliveryId()', () => {
+  // it('should', () => {
+  //
+  // });
+});
