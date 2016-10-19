e96c54a3f266fbcb0e48fdf0f2e308d580a40b23
Nick Krambousanos <nkrambo@gmail.com>
2016-10-18 17:00:41 -0700

    rect

--- math/questions/rectangular_intersection/rectangular_intersection.js
+++ math/questions/rectangular_intersection/rectangular_intersection.js
@@ -4,6 +4,7 @@
 *
 * Types: Maths, Rectangle Intersection
 *
+*                                   h
 *                                   +------------------+
 *                                   |                  |
 *                                   |                  |
@@ -12,16 +13,16 @@
 *                                   |                  |
 *                                   |                  |
 *                                   |                  |
-*                                   |                  |
+*          h                        |                  |
 *          +--------------------------------------+    |
 *          |                        |+ + + + + + +|    |
 *          |                        |+ + + + + + +|    |
 *          |                        |+ + + + + + +|    |
 *          |                        |+ + + + + + +|    |
 *          |                        +------------------+
-*          |                                      |
+*          |                      (x, y)          |    w
 *          +--------------------------------------+
-*
+*       (x, y)                                    w
 *
 * Write a function to find the rectangular intersection of two given rectangles.
 *
@@ -81,6 +82,39 @@
 *
 * We can write a helper function to return this.
 *
+* Let's start with the first 2 cases.
+*
+* One of our ranges starts 'further to the right' than the other. We don't know
+* ahead of time which one it is, but we can check the starting points of each range
+* to see which one has the highestStartPoint. That highestStartPoint is always
+* the left-hand side of the overlap, if there is one.
+*
+* Similarly, the right-hand side of our overlap is always the lowestEndPoint.
+* That may or may not be the end point of the same input range that had the
+* highestStartPoint — compare cases (1) and (2).
+*
+* This gives us our x overlap! So we can handle cases (1) and (2). How do we know
+* when there is no overlap?
+*
+* If highestStartPoint > lowestEndPoint, the two rectangles do not overlap.
+*
+* But be careful — is it just greater than or is it greater than or equal to?
+*
+* It depends how we want to handle case (4) above.
+*
+* If we use greater than, we treat case (4) as an overlap. This means we could
+* end up returning a rectangle with zero width. Let's say a rectangle with zero
+* width (or zero height) isn't a rectangle at all, so we should treat that case
+* as 'no intersection'.
+*
+* We write our rangeOverlap() helper function.
+*
+* We use this helper to calculate the overlap length of both the x and y axis',
+* then build the rectangular overlap.
+*
+* Both problems are basically the same as finding the intersection of two 'ranges'
+* on a 1 - dimensional number line.
+*
 * Time: O(1)
 * Space: O(1)
 *
@@ -89,7 +123,7 @@
 * @return {object} returns a rectangle object, representing the intersection of x and y
 */
 
-function findRangeOverlap(point1, length1, point2, length2) {
+function rangeOverlap(point1, length1, point2, length2) {
 
   // find the highest start point and lowest end point.
   // the highest ("rightmost" or "upmost") start point is
@@ -111,8 +145,8 @@ function findRangeOverlap(point1, length1, point2, length2) {
 
 function rectangleIntersection(rect1, rect2) {
   // get the x and y overlap points and lengths
-  const xOverlap = findRangeOverlap(rect1.leftX, rect1.width, rect2.leftX, rect2.width);
-  const yOverlap = findRangeOverlap(rect1.bottomY, rect1.height, rect2.bottomY, rect2.height);
+  const xOverlap = rangeOverlap(rect1.leftX, rect1.width, rect2.leftX, rect2.width);
+  const yOverlap = rangeOverlap(rect1.bottomY, rect1.height, rect2.bottomY, rect2.height);
 
   // return null rectangle if there is no overlap
   if (!xOverlap.overlapLength || !yOverlap.overlapLength) {
--- math/questions/rectangular_intersection/test/rectangular_intersection.spec.js
+++ math/questions/rectangular_intersection/test/rectangular_intersection.spec.js
@@ -7,37 +7,57 @@ describe('rectangleIntersection()', () => {
     const rect1 = {
       // coordinates of bottom-left corner
       leftX: 1,
-      bottomY: 5,
+      bottomY: 1,
 
       // width and height
       width: 10,
-      height: 4
+      height: 5
     };
 
     const rect2 = {
       // coordinates of bottom-left corner
-      leftX: 1,
-      bottomY: 5,
+      leftX: 3,
+      bottomY: 3,
 
       // width and height
       width: 10,
-      height: 4
+      height: 5
     };
 
     const intersection = {
       // coordinates of bottom-left corner
-      leftX: 1,
-      bottomY: 5,
+      leftX: 3,
+      bottomY: 3,
 
       // width and height
-      width: 10,
-      height: 4
+      width: 8,
+      height: 3
     };
 
     expect(rectangleIntersection(rect1, rect2)).to.deep.equal(intersection);
   });
 
   it('should return an object with properties set to null if there\'s no intersection', () => {
+    const rect1 = {
+      // coordinates of bottom-left corner
+      leftX: 1,
+      bottomY: 1,
+
+      // width and height
+      width: 4,
+      height: 2
+    };
+
+    const rect2 = {
+      // coordinates of bottom-left corner
+      leftX: 5,
+      bottomY: 5,
+
+      // width and height
+      width: 4,
+      height: 2
+    };
+
     const nullRect = {
       leftX: null,
       bottomY: null,
@@ -45,6 +65,6 @@ describe('rectangleIntersection()', () => {
       height: null,
     };
 
-    expect(rectangleIntersection()).to.deep.equal(nullRect);
+    expect(rectangleIntersection(rect1, rect2)).to.deep.equal(nullRect);
   });
 });
