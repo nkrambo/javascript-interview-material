3cb033759047e6ad03dcb373d9037ede49d17c5b
Nick Krambousanos <nkrambo@gmail.com>
2016-10-28 23:31:23 -0700

    stub alien dictionary

---
+++ assets/arrays/questions/__alien_dictionary/alien_dictionary.js
@@ -0,0 +1,41 @@
+
+/**
+* Alien Dictionary
+*
+* Types:
+*
+* There is a new alien language which uses the latin alphabet. However, the order
+* among letters are unknown to you. You receive a list of words from the dictionary,
+* where words are sorted lexicographically by the rules of this new language.
+* Derive the order of letters in this language.
+*
+* For example, given the following words in dictionary:
+*
+* [
+*   'wrt',
+*   'wrf',
+*   'er',
+*   'ett',
+*   'rftt'
+* ]
+*
+* The correct order is: 'wertf'.
+*
+* Notes:
+*
+* 1. You may assume all letters are in lowercase.
+* 2. If the order is invalid, return an empty string.
+* 3. There may be multiple valid order of letters, return any one of them is fine.
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {array} words array of strings
+* @return {string} returns a string representing the order of the words
+*/
+
+function alienOrder(words) {
+
+}
+
+export default alienOrder;
