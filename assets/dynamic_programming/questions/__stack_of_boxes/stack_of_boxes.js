0e3c9b5a7f40929f71cace68a6f537623e6eadca
Nick Krambousanos <nkrambo@gmail.com>
2016-10-28 18:32:30 -0700

    stub stack of boxes

---
+++ assets/dynamic_programming/questions/__stack_of_boxes/stack_of_boxes.js
@@ -0,0 +1,24 @@
+
+/**
+* Stack of Boxes
+*
+* You have a stack of n boxes, with widths w(i), heights h(i), and depths d(i).
+* The boxes cannot be rotated and can only be stacked on top of one another if
+* each box in the stack is strictly larger than the box above it in width, height,
+* and depth. Implement a function to compute the height of the tallest possible
+* stack. The height of a stack is the sum of the heights of each box.
+*
+* Solution:
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} string of unique characters
+* @return {array} returns array of strings of all permutations
+*/
+
+function tallestStack(boxes) {
+
+}
+
+export default tallestStack;
---
+++ assets/dynamic_programming/questions/__stack_of_boxes/test/stack_of_boxes.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import tallestStack from '../stack_of_boxes';
+
+describe('tallestStack()', () => {
+  // it('should', () => {
+  //
+  // });
+});
