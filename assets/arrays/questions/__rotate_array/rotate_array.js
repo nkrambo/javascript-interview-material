030e6bfcbb306c9c10b32a50dc3d8cfa4f80eabf
Nick Krambousanos <nkrambo@gmail.com>
2016-11-02 16:30:13 -0700

    update rotate array

--- assets/arrays/questions/__rotate_array/rotate_array.js
+++ assets/arrays/questions/__rotate_array/rotate_array.js
@@ -116,4 +116,88 @@ function rotateArrayExtra(nums, k) {
   }
 }
 
-export { rotateArrayBrute, rotateArrayExtra };
+/**
+* rotateArrayCyclic()
+*
+* Solution:
+*
+*
+*
+* Time: O(n)
+* Space: O(1)
+*
+*
+*
+* @param {array} nums array of integers
+* @param {number} k number of steps to rotate
+* @return {void} modify nums in-place
+*/
+
+function rotateArrayCyclic(nums, k) {
+
+}
+
+/**
+* rotateArrayReverse()
+*
+* Solution:
+*
+* This approach is based on the fact that when we rotate the array k times, k elements
+* from the back end of the array come to the front and the rest of the elements from
+* the front shift backwards.
+*
+* In this approach, we firstly reverse all the elements of the array. Then, reversing
+* the first k elements followed by reversing the rest n-k elements gives us the
+* required result.
+*
+* Let n = 7 and k = 3.
+*
+* Original List                       : 1 2 3 4 5 6 7
+* After reversing all numbers         : 7 6 5 4 3 2 1
+* After reversing first k numbers     : 5 6 7 4 3 2 1
+* After reversing last n - k numbers  : 5 6 7 1 2 3 4 => Result
+*
+* Time: O(n)
+* Space: O(1)
+*
+* n elements are reversed a total of 3 times. No additional space is needed.
+*
+* @param {array} nums array of integers
+* @param {number} k number of steps to rotate
+* @return {void} modify nums in-place
+*/
+
+function rotateArrayReverse(nums, k) {
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1); // reverse all
+  reverse(nums, 0, k - 1);           // reverse k numbers
+  reverse(nums, k, nums.length - 1); // reverse n - k numbers
+}
+
+/**
+* reverse()
+*
+* Helper method to reverse an array.
+*
+* Time: O(n)
+* Space: O(1)
+*
+* n elements are reversed.
+*
+* @param {array} nums array of integers
+* @param {number} start index
+* @param {number} end index
+* @return {void} modify nums in-place
+*/
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start += 1;
+    end -= 1;
+  }
+}
+
+export { rotateArrayBrute, rotateArrayExtra, rotateArrayCyclic, rotateArrayReverse };
--- assets/arrays/questions/__rotate_array/test/rotate_array.spec.js
+++ assets/arrays/questions/__rotate_array/test/rotate_array.spec.js
@@ -1,6 +1,6 @@
 
 import { expect } from 'chai';
-import { rotateArrayBrute, rotateArrayExtra } from '../rotate_array';
+import { rotateArrayBrute, rotateArrayExtra, rotateArrayCyclic, rotateArrayReverse } from '../rotate_array';
 
 describe('rotateArrayBrute()', () => {
   it('should rotate an array to the right by k steps', () => {
@@ -14,6 +14,22 @@ describe('rotateArrayExtra()', () => {
   it('should rotate an array to the right by k steps', () => {
     const test = [1, 2, 3, 4, 5, 6, 7];
     rotateArrayExtra(test, 3);
+    expect(test).to.deep.equal([5, 6, 7, 1, 2, 3, 4]);
+  });
+});
+
+// describe('rotateArrayCyclic()', () => {
+//   it('should rotate an array to the right by k steps', () => {
+//     const test = [1, 2, 3, 4, 5, 6, 7];
+//     rotateArrayCyclic(test, 3);
+//     expect(test).to.deep.equal([5, 6, 7, 1, 2, 3, 4]);
+//   });
+// });
+
+describe('rotateArrayReverse()', () => {
+  it('should rotate an array to the right by k steps', () => {
+    const test = [1, 2, 3, 4, 5, 6, 7];
+    rotateArrayReverse(test, 3);
     expect(test).to.deep.equal([5, 6, 7, 1, 2, 3, 4]);
   });
 });
