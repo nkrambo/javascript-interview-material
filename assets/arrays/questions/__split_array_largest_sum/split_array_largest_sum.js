1b428bc4264c0cd6fddaa07d721057e5377f85c5
Nick Krambousanos <nkrambo@gmail.com>
2016-11-03 11:50:45 -0700

    stub bit manipulation

--- assets/arrays/questions/__split_array_largest_sum/split_array_largest_sum.js
+++ assets/arrays/questions/__split_array_largest_sum/split_array_largest_sum.js
@@ -2,13 +2,16 @@
 /**
 * Split Arary Largest Sum
 *
+* Types: Binary Search, Greedy
+*
 * Given an array which consists of non-negative integers and an integer m, you
-* can split the array into m non-empty continuous subarrays. Write an algorithm
+* can split the array into m non-empty continuous sub arrays. Write an algorithm
 * to minimize the largest sum among these m subarrays.
 *
 * Example:
 * Input: [7, 2, 5, 10, 8], 2
 * Output: 18
+*
 * There are four ways to split nums into two subarrays. The best way is to split
 * it into [7, 2, 5] and [10, 8], where the largest sum among the two subarrays
 * is only 18.
@@ -19,6 +22,8 @@
 *
 * Solution:
 *
+* 
+*
 * Time: O(1)
 * Space: O(1)
 *
@@ -28,7 +33,50 @@
 */
 
 function splitArray(nums, m) {
-  
+  let max = 0;
+  let sum = 0;
+
+  // edge case
+  if (m === 1) return sum;
+
+  for (let num = 0; num < nums.length; num += 1) {
+    max = Math.max(num, max);
+    sum += num;
+  }
+
+  //binary search
+  let left = max;
+  let right = sum;
+
+  while (left <= right) {
+    const mid = (left + right) / 2;
+
+    if (valid(mid, nums, m)) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
+
+function valid(target, nums, m) {
+  let count = 1;
+  let total = 0;
+
+  for (let num = 0; num < nums.length; num +=1 ) {
+    total += num;
+
+    if (total > target) {
+      total = num;
+      count += 1;
+
+      if (count > m) return false;
+    }
+  }
+
+  return true;
 }
 
 export default splitArray;
--- assets/arrays/questions/__split_array_largest_sum/test/split_array_largest_sum.spec.js
+++ assets/arrays/questions/__split_array_largest_sum/test/split_array_largest_sum.spec.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import splitArray from '../split_array_largest_sum';
 
 describe('splitArray()', () => {
-  // it('should', () => {
-  //
-  // });
+  it('should split an array into m non-empty continuous sub arrays', () => {
+    expect(splitArray([7, 2, 5, 10, 8], 2)).to.equal(18);
+  });
 });
