bbda7da8f9db5230856c71bfeb8762872b599f0e
Nick Krambousanos <nkrambo@gmail.com>
2016-11-03 22:15:45 -0700

    update flip bit

--- assets/bit_manipulation/common_tasks/update_bit/update_bit.js
+++ assets/bit_manipulation/common_tasks/update_bit/update_bit.js
@@ -7,6 +7,29 @@
 * i bits. This will create a number with bit i equal to v and all other bits equal
 * to 0. Finally, we OR these two numbers, updating the ith bit if v is 1 leaving it
 * as 0 otherwise.
+
+
+
+* Example:
+* Input: 6, 3, true
+* Output: 14
+*
+* So, our input represented as their binary numbers are:
+*
+* 00000110   num (6)
+* 00000011   i (3)
+*
+* We set our value to either 1 or 0 depending on our third argument.
+*
+* We create a mask by shifting 1 over by i bits, represented as (1 << i) but then
+* we negate it using the NOT operand '~'.
+*
+* 00001000   normal mask (1 << i)
+* 11110111   negated mask
+*
+* Then if we simply AND together our num and negated mask to clear the bit at i.
+*
+* Finally, we can OR our cleared number with the ith bit.
 *
 * Time: O(1)
 * Space: O(1)
