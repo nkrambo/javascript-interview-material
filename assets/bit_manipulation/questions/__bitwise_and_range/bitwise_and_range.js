598c8154b754c753c1021d68bc2e69281f43fe1d
Nick Krambousanos <nkrambo@gmail.com>
2016-11-05 14:11:21 -0700

    stub bitwise range numbers

---
+++ assets/bit_manipulation/questions/__bitwise_and_range/bitwise_and_range.js
@@ -0,0 +1,26 @@
+
+/**
+* Bitwise AND of Numbers Range
+*
+* Types: Bit Manipulation
+*
+* Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND
+* of all numbers in this range, inclusive.
+*
+* Example:
+* Input: [5, 7]
+* 4
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} m 32-bit unsigned integer
+* @param {number} n 32-bit unsigned integer
+* @return {number} Returns the bitwise AND of all numbers between m and n
+*/
+
+function bitwiseAndRange(m, n) {
+
+}
+
+export default bitwiseAndRange;
---
+++ assets/bit_manipulation/questions/__bitwise_and_range/test/bitwise_and_range.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import bitwiseAndRange from '../bitwise_and_range';
+
+describe('bitwiseAndRange()', () => {
+  // it('should return the bitwise AND of all numbers between m and n', () => {
+  //   expect(bitwiseAndRange([5, 7])).to.equal(4);
+  // });
+});
