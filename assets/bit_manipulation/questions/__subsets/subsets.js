e90a83489d02f66364f56fb763ebb8d8ecb19d19
Nick Krambousanos <nkrambo@gmail.com>
2016-11-05 14:03:05 -0700

    stub subsets

---
+++ assets/bit_manipulation/questions/__subsets/subsets.js
@@ -0,0 +1,33 @@
+
+/**
+* Subsets
+*
+* Types: Bit Manipulation
+*
+* Given a set of distinct integers, nums, return all possible subsets.
+*
+* Note: The solution set must not contain duplicate subsets.
+*
+* Example:
+* Input: [1, 2, 3]
+* Output: [[3],
+*          [1],
+*          [2],
+*          [1, 2, 3],
+*          [1, 3],
+*          [2, 3],
+*          [1, 2],
+*          []]
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {array} nums array of distinct integers
+* @return {array} Returns an array of all possible subsets of nums
+*/
+
+function subsets(num) {
+
+}
+
+export default subsets;
