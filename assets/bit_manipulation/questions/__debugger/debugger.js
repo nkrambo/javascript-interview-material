769c85ca3c72de76977197afe5004b61107539e1
Nick Krambousanos <nkrambo@gmail.com>
2016-11-04 23:52:15 -0700

    stub

--- assets/bit_manipulation/questions/__debugger/debugger.js
+++
@@ -1,8 +0,0 @@
-
-/**
-* Debugger
-*
-* Explain what the following code does: ((n & (n - 1)) == 0)
-*
-*/
-
---
+++ assets/bit_manipulation/questions/__power_of_two/power_of_two.js
@@ -0,0 +1,20 @@
+
+/**
+* Power of Two
+*
+* Types: Bit Manipulation
+*
+* Given an integer, write a function to determine if it is a power of two.
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} num to check is power of two
+* @return {boolean} Returns true if num is a power of 2, otherwise false
+*/
+
+function powerOfTwo(num) {
+
+}
+
+export default powerOfTwo;
