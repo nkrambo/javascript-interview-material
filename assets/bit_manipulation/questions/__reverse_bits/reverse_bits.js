a91170b03574d5931c60a97cc42e0bc5d210d34a
Nick Krambousanos <nkrambo@gmail.com>
2016-11-05 00:10:17 -0700

    stub reverse bits

---
+++ assets/bit_manipulation/questions/__reverse_bits/reverse_bits.js
@@ -0,0 +1,27 @@
+
+/**
+* Reverse Bits
+*
+* Types: Bit Manipulation
+*
+* Reverse bits of a given 32 bits unsigned integer.
+*
+* Example:
+* Input: 43261596 (represented in binary as 00000010100101000001111010011100)
+* Output: 964176192 (represented in binary as 00111001011110000010100101000000)
+*
+* Follow up:
+* If this function is called many times, how would you optimize it?
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} num 32-bit unsigned integer
+* @return {number} Returns the reversed of num
+*/
+
+function reverseBits(num) {
+
+}
+
+export default reverseBits;
