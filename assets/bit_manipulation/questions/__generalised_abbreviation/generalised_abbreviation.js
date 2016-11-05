eb5a4fdc44a0057f4e0092de8822a4451c545403
Nick Krambousanos <nkrambo@gmail.com>
2016-11-05 14:28:44 -0700

    stub out generalised abbreviatiions

---
+++ assets/bit_manipulation/questions/__generalised_abbreviation/generalised_abbreviation.js
@@ -0,0 +1,40 @@
+
+/**
+* Generalised Abbreviation
+*
+* Types: Bit Manipulation
+*
+* Write a function to generate the generalized abbreviations of a word. Order does
+* not matter.
+*
+* Example:
+* Input: 'word'
+* Output: ['word',
+*          '1ord',
+*          'w1rd',
+*          'wo1d',
+*          'wor1',
+*          '2rd',
+*          'w2d',
+*          'wo2',
+*          '1o1d',
+*          '1or1',
+*          'w1r1',
+*          '1o2',
+*          '2r1',
+*          '3d',
+*          'w3',
+*          '4']
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} word a word to generate abbreviations from
+* @return {array} Returns an array of strings representing the abbreviations of word
+*/
+
+function generateAbbreviations(word) {
+
+}
+
+export default generateAbbreviations;
