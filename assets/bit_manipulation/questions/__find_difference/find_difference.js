9ee195923fe83691c3bba57f945df5bcd05bbe43
Nick Krambousanos <nkrambo@gmail.com>
2016-11-05 00:52:48 -0700

    stub find difference

---
+++ assets/bit_manipulation/questions/__find_difference/find_difference.js
@@ -0,0 +1,31 @@
+
+/**
+* Find the Difference
+*
+* Types: Bit Manipulation
+*
+* Given two strings s1 and s2 which consist of only lowercase letters.
+*
+* String s2 is a random shuffling of string s1 but with one more letter at a
+* random position.
+*
+* Find the random letter that was added in s2.
+*
+* Example:
+* Input: 'abcd', 'abcde'
+* Output: 'e'
+* 'e' is the letter that was added.
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {string} s1 string of n length
+* @param {string} s2 a random shuffling of s1 with an additional random letter
+* @return {number} Returns the random letter from s2
+*/
+
+function findDifference(s1, s2) {
+
+}
+
+export default findDifference;
---
+++ assets/bit_manipulation/questions/__find_difference/test/find_difference.spec.js
@@ -0,0 +1,9 @@
+
+import { expect } from 'chai';
+import findDifference from '../find_difference';
+
+describe('findDifference()', () => {
+  // it('should return the random letter from s2, that is not in s1', () => {
+  //   expect(findDifference('abcd', 'abecd')).to.equal('e');
+  // });
+});
