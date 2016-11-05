7da0705c2bec99fd2757d12cea7dae3a9791faff
Nick Krambousanos <nkrambo@gmail.com>
2016-11-05 14:16:40 -0700

    stub power of four

---
+++ assets/bit_manipulation/questions/__power_of_four/power_of_four.js
@@ -0,0 +1,32 @@
+
+/**
+* Power of Four
+*
+* Types: Bit Manipulation
+*
+* Given an integer (signed 32 bits), write a function to check whether it is a
+* power of 4.
+*
+* Example:
+* Input: 16
+* Output: true
+*
+* Example:
+* Input: 5
+* Output: false
+*
+* Follow up:
+* Could you solve it without loops/recursion?
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} num 32-bit unsigned integer
+* @return {boolean} Returns true if num is a power of 4, otherwise false
+*/
+
+function powerOfFour(num) {
+
+}
+
+export default powerOfFour;
---
+++ assets/bit_manipulation/questions/__power_of_four/test/power_of_four.spec.js
@@ -0,0 +1,13 @@
+
+import { expect } from 'chai';
+import powerOfFour from '../power_of_four';
+
+describe('powerOfFour()', () => {
+  it('should return true if num is a power of four', () => {
+    expect(powerOfFour(16)).to.be.true;
+  });
+
+  it('should return false if num is not a power of four', () => {
+    expect(powerOfFour(5)).to.be.false;
+  });
+});
