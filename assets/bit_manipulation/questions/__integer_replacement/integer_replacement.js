a191545a7a2d0d39277202b8aea62f3c78e3bfbc
Nick Krambousanos <nkrambo@gmail.com>
2016-11-06 15:08:20 -0800

    stub integer replacement

---
+++ assets/bit_manipulation/questions/__integer_replacement/integer_replacement.js
@@ -0,0 +1,35 @@
+
+/**
+* Integer Replacement
+*
+* Types: Bit Manipulation
+*
+* Given a positive integer num and you can do operations as follow:
+*
+* 1. If num is even, replace num with num / 2.
+* 2. If num is odd, you can replace num with either num + 1 or num - 1.
+*
+* What is the minimum number of replacements needed for num to become 1?
+*
+* Example:
+* Input: 8
+* Output: 3
+* 8 -> 4 -> 2 -> 1
+*
+* Example:
+* Input: 7
+* Output: 4
+* 7 -> 8 -> 4 -> 2 -> 1 or 7 -> 6 -> 3 -> 2 -> 1
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} num 32-bit unsigned integer
+* @return {number} Returns calculated the minimum replacements for num to equal 1
+*/
+
+function integerReplacement(num) {
+
+}
+
+export default integerReplacement;
