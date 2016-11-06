2f7fb4bf0c8bae1df19bdfc4dc74e76506cad627
Nick Krambousanos <nkrambo@gmail.com>
2016-11-06 14:45:48 -0800

    stub number to hexadecimal

---
+++ assets/bit_manipulation/questions/__number_to_hexadecimal/number_to_hexadecimal.js
@@ -0,0 +1,42 @@
+
+/**
+* Convert Number to Hexadecimal
+*
+* Types: Bit Manipulation
+*
+* Given an integer, write an algorithm to convert it to hexadecimal. For negative
+* integer, two's complement method is used.
+*
+* Note:
+*
+* 1. All letters in hexadecimal (a-f) must be in lowercase.
+*
+* 2. The hexadecimal string must not contain extra leading 0s. If the number is
+*    zero, it is represented by a single zero character '0'; otherwise, the first
+*    character in the hexadecimal string will not be the zero character.
+*
+* 3. The given number is guaranteed to fit within the range of a 32-bit signed integer.
+*
+* 4. You must not use any method provided by the library which converts/formats
+*    the number to hex directly.
+*
+* Example:
+* Input: 26
+* Output: '1a'
+*
+* Example:
+* Input: -1
+* Output: 'ffffffff'
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} num 32-bit integer
+* @return {string} Returns the hexadecimal value of num
+*/
+
+function numberToHex(num) {
+
+}
+
+export default numberToHex;
