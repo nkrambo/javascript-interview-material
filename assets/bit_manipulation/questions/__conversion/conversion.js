52b95913d556f34322aaf099d45566436c0429e2
Nick Krambousanos <nkrambo@gmail.com>
2016-11-12 16:49:50 -0800

    conversion

--- assets/bit_manipulation/questions/__conversion/conversion.js
+++ assets/bit_manipulation/questions/__conversion/conversion.js
@@ -12,12 +12,9 @@
 *
 * Solution:
 *
-*/
-
-/**
-* Conversion
 *
-* Time:
+* Time: O(1)
+* Space: O(1)
 *
 * @param {number} a integer, base
 * @param {number} b integer to convert a to
@@ -25,7 +22,13 @@
 */
 
 function conversion(a, b) {
+  let count = 0;
 
+  for (let c = a ^ b; c !== 0; c = c & (c - 1)) {
+    count += 1;
+  }
+
+  return count;
 }
 
 export default conversion;
--- assets/bit_manipulation/questions/__conversion/test/conversion.spec.js
+++ assets/bit_manipulation/questions/__conversion/test/conversion.spec.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import conversion from '../conversion';
 
 describe('conversion()', () => {
-  // it('should', () => {
-  //
-  // });
+  it('should return the number of bit flips required to convert integer a to b.', () => {
+    expect(conversion(29, 15)).to.equal(2);
+  });
 });
