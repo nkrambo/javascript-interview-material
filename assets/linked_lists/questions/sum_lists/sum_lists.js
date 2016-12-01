9f71595fadab833fb3bbed9c1f37ea53b5adcf8c
Nick Krambousanos <nkrambo@gmail.com>
2016-12-01 13:49:29 -0800

    sum lists

--- assets/linked_lists/questions/sum_lists/sum_lists.js
+++ assets/linked_lists/questions/sum_lists/sum_lists.js
@@ -61,10 +61,28 @@ import LinkedList from '../../class/linked_list/linked_list';
 * essentially have a '0' in this column for the top number. So, (0 + 2 = 2).
 *
 * It's also possible in some instances that we need to carry our final value over.
-* 
+* For example:
+*
+*     6 1 7
+*   + 5 9 5
+*   -------
+*   1 2 1 2
+*
+* Notice that we carry a '1' from the 100s column to calculate our final sum.
+*
+* Knowing this long addition process, we can build out our solution... We can break
+* this into 3 steps:
+*
+* - 'matched' values; columns with 2 values
+* - 'single' values; columns with 1 value
+* - last remainder; if final remainder is above 0
 *
 * Time: O(n)
-* Space: O(1)
+* Space: O(log n)
+*
+* Our time complexity is O(n), where (n) is the total nodes in l1 + l2. Our space
+* complexity will be O(log n) as we need to return a new list, which will be about
+* half the nodes of (n).
 *
 * @param {object} l1 linked list
 * @param {object} l2 linked list
@@ -73,16 +91,19 @@ import LinkedList from '../../class/linked_list/linked_list';
 
 function sumLists(l1, l2) {
   const results = new LinkedList();
-
   let node1 = l1.head;
   let node2 = l2.head;
   let carry = 0;
   let sum;
 
-  // add up matched nodes
+  // matched values:
+  // iterate while we have both values in columns
   while (node1 && node2) {
+
+    // add up values and carry
     sum = node1.value + node2.value + carry;
 
+    // check for carry value
     if (sum >= 10) {
       carry = 1;
       sum -= 10;
@@ -90,16 +111,23 @@ function sumLists(l1, l2) {
       carry = 0;
     }
 
+    // add sum to result list
     results.add(sum, results.length);
+
+    // keep iterating
     node1 = node1.next;
     node2 = node2.next;
   }
 
+  // single values:
   // get any remaining values
   let node = node1 || node2;
 
+  // iterate remaining values
   while (node) {
     sum = node.value + carry;
+
+    // check for carry
     if (sum >= 10) {
       carry = 1;
       sum -= 10;
@@ -107,11 +135,12 @@ function sumLists(l1, l2) {
       carry = 0;
     }
 
+    // add to sum and look for next single value
     results.add(sum, results.length);
     node = node.next;
   }
 
-  // check for any remainder
+  // check for any last remainder
   if (carry > 0) results.add(carry, results.length);
 
   return results;
