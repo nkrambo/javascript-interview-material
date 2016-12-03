72d1d200449665d58664cfdff23471880508c603
Nick Krambousanos <nkrambo@gmail.com>
2016-12-03 14:17:39 -0800

    loop detection

--- assets/linked_lists/questions/__loop_detection/test/loop_detection.spec.js
+++ assets/linked_lists/questions/__loop_detection/test/loop_detection.spec.js
@@ -4,25 +4,28 @@ import LinkedList from '../../../class/linked_list/linked_list';
 import { loopDetectionSet, loopDetectionRunner } from '../loop_detection';
 
 describe('loopDetectionSet()', () => {
-
-  // A -> B -> C -> D -> E -> C [the same C as eariler, so loop exists]
-  const l1 = new LinkedList();
-  let a = 'a';
-  let b = 'b';
-  let c = 'c';
-  let d = 'd';
-  let e = 'e';
-
-  l1.add(c, 0);
-  l1.add(e, 0);
-  l1.add(d, 0);
-  l1.add(c, 0);
-  l1.add(b, 0);
-  l1.add(a, 0);
-
   it('should return the node at the beginning of a loop if a loop exists', () => {
+    // A -> B -> C -> D -> E -> C [the same C as eariler, so loop exists]
+    const l1 = new LinkedList();
+    let a = 'a';
+    let b = 'b';
+    let c = 'c';
+    let d = 'd';
+    let e = 'e';
+
+    l1.add(c, 0);
+    l1.add(e, 0);
+    l1.add(d, 0);
+    l1.add(c, 0);
+    l1.add(b, 0);
+    l1.add(a, 0);
+
     const loopNode = loopDetectionSet(l1);
     expect(loopNode.value).to.equal('c');
+  });
+
+  it('should return null if there is no loop', () => {
+
   });
 });
 
