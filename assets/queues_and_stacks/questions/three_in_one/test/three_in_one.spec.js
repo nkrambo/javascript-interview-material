753646066b5c997180093380db7b2dabfe5a3eec
Nick Krambousanos <nkrambo@gmail.com>
2016-12-06 13:50:05 -0800

    three in one

--- assets/queues_and_stacks/questions/three_in_one/test/three_in_one.spec.js
+++ assets/queues_and_stacks/questions/three_in_one/test/three_in_one.spec.js
@@ -3,7 +3,67 @@ import { expect } from 'chai';
 import ThreeInOne from '../three_in_one';
 
 describe('ThreeInOne', () => {
-  // it('should', () => {
-  //
-  // });
+  it('should construct a ThreeInOne class', () => {
+    const tripleStack = new ThreeInOne();
+
+    expect(tripleStack.array).to.a('array');
+    expect(tripleStack.array).to.be.empty;
+
+    expect(tripleStack.lengths.length).to.equal(3);
+    expect(tripleStack.lengths[0]).to.equal(0);
+  });
+
+  it('should return push a frame to 3 different stacks', () => {
+    const tripleStack = new ThreeInOne();
+    tripleStack.push(1, 1);
+    tripleStack.push(1, 2);
+    tripleStack.push(1, 3);
+    tripleStack.push(2, 1);
+    tripleStack.push(3, 1);
+
+    expect(tripleStack.array[0]).to.equal(1);
+    expect(tripleStack.array[1]).to.equal(1);
+    expect(tripleStack.array[2]).to.equal(1);
+    expect(tripleStack.array[3]).to.equal(2);
+    expect(tripleStack.array[4]).to.equal(undefined);
+  });
+
+  it('should return the length of the 3 different stacks', () => {
+    const tripleStack = new ThreeInOne();
+    tripleStack.push(1, 1);
+    tripleStack.push(1, 2);
+    tripleStack.push(3, 1);
+
+    expect(tripleStack.getLength(1)).to.equal(2);
+    expect(tripleStack.getLength(3)).to.equal(1);
+    expect(tripleStack.getLength(2)).to.equal(0);
+  });
+
+  it('should return pop a frame from 3 different stacks', () => {
+    const tripleStack = new ThreeInOne();
+    tripleStack.push(1, 1);
+    tripleStack.push(1, 2);
+
+    expect(tripleStack.pop(1)).to.equal(2);
+    expect(tripleStack.pop(2)).to.equal(null);
+  });
+
+  it('should return peek values from 3 different stacks', () => {
+    const tripleStack = new ThreeInOne();
+    tripleStack.push(1, 1);
+    tripleStack.push(1, 2);
+
+    expect(tripleStack.peek(1)).to.equal(2);
+    expect(tripleStack.peek(2)).to.equal(null);
+  });
+
+  it('should check if 3 different stacks are empty', () => {
+    const tripleStack = new ThreeInOne();
+    tripleStack.push(1, 1);
+    tripleStack.push(3, 1);
+
+    expect(tripleStack.isEmpty(1)).to.be.false;
+    expect(tripleStack.isEmpty(3)).to.be.false;
+    expect(tripleStack.isEmpty(2)).to.be.true;
+  });
 });
--- assets/queues_and_stacks/questions/three_in_one/three_in_one.js
+++ assets/queues_and_stacks/questions/three_in_one/three_in_one.js
@@ -5,19 +5,55 @@
 * Solution:
 *
 * We can write a class that holds 3 stacks in one array. This is done by interleaving
-* the values from the 3 indexes, so the first items are at 0, 1 and 2 and
-* subsequent items are every 3 places from those.
+* the values from the 3 indices, so the first items are at 0, 1 and 2 and subsequent
+* items are every 3 places from those.
+*
+* This means that we need to calculate our index on our push, pop and peek methods.
+*
+* On push(), our index will always be:
+*
+* length * 3 + (stack - 1)
+*
+* This makes sense when you think about it. We're always increasing our index by
+* 3 and then we have to add where our stack is, which is a zero based array, hence
+* (stack - 1).
+*
+* You can see this in practice below... We insert at index 0, for every 3 index
+* blocks.
+*
+* const tripleStack = new ThreeInOne();
+*
+* tripleStack.push(1, 1);
+* tripleStack.push(1, 2);
+* tripleStack.push(1, 3);
+* tripleStack.push(1, 4);
+*
+* tripleStack {
+*   array: [1, undefined, undefined, 2, undefined, undefined, 3, undefined, undefined, 4],
+*   lengths: [4, 0, 0]
+* }
 *
 * This class takes advantage of the fact that JavaScript arrays are dynamic and
-* doesn't hold the stacks to any size. It doesn't reduce the size of the underlying
+* so our stacks can grow to any size. It doesn't reduce the size of the underlying
 * array when items are popped but that could easily be added.
+*
+* On pop() and peek(), calculating our index will be a little different. We want
+* to grab the 'top' frame, so we need to calculate our last insert, we can do that
+* like so:
+*
+* (length - 1) * 3 + (stack - 1)
+*
+* The only difference being that we subtract '1' from length to get the last 'block'
+* of 3 indices. Then we add our 'stack' index.
+*
+* All methods run in constant time.
 */
 
 class ThreeInOne {
+
   /**
-  * All stacks use a single array
-  We track lengths of stacks in an array.
-  *
+  * All stacks use a single array to store values.
+  * We track lengths of stacks in separate array.
   *
   * @constructor
   */
@@ -28,22 +64,7 @@ class ThreeInOne {
   }
 
   /**
-  * getLength()
-  *
-  * text
-  *
-  * @param {number} stack which stack length
-  * @return {number} the length of stack
-  */
-
-  getLength(stack) {
-    return this.lengths[stack - 1];
-  }
-
-  /**
   * push()
-  *
-  * text
   *
   * @param {number} stack to push frame to
   * @param {*} value to push
@@ -60,9 +81,18 @@ class ThreeInOne {
   }
 
   /**
-  * pop()
+  * getLength()
   *
-  * text
+  * @param {number} stack which stack length
+  * @return {number} the length of stack
+  */
+
+  getLength(stack) {
+    return this.lengths[stack - 1];
+  }
+
+  /**
+  * pop()
   *
   * @param {number} stack to pop frame from
   * @return {*} returns the top value from stack and removes frame
@@ -71,21 +101,21 @@ class ThreeInOne {
   pop(stack) {
     const length = this.getLength(stack);
 
-    if (length > 0) {
-      const index = (length - 1) * 3 + stack - 1;
-      const value = this.array[index];
+    if (length === 0) return null;
 
-      this.array[index] = undefined;
-      this.lengths[stack - 1] -= 1;
-    }
+    // grab index and value
+    const index = (length - 1) * 3 + stack - 1;
+    const value = this.array[index]; // cache
+
+    // 'delete' value and decrement length
+    this.array[index] = undefined;
+    this.lengths[stack - 1] -= 1;
 
     return value;
   }
 
   /**
   * peek()
-  *
-  * text
   *
   * @param {number} stack to peek value from
   * @return {*} returns the top value from stack without removing frame
@@ -94,12 +124,12 @@ class ThreeInOne {
   peek(stack) {
     const length = this.getLength(stack);
 
-    if (length > 0) {
-      const index = (length - 1) * 3 + stack - 1;
-      const value = this.array[index];
-    }
+    if (length === 0) return null;
 
-    return value;
+    // grab index and value
+    const index = (length - 1) * 3 + stack - 1;
+
+    return this.array[index];
   }
 
   /**
