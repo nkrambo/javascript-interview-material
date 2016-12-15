2229bff5e6006264d6ab45a412e495a1fb5317da
Nick Krambousanos <nkrambo@gmail.com>
2016-12-15 01:28:53 -0800

    tarjan

--- assets/trees_and_graphs/common/shortest_path/dijkstra.js
+++ assets/trees_and_graphs/common/shortest_path/dijkstra.js
@@ -2,12 +2,7 @@
 /**
 * Dijkstra's Algorithm
 *
-* 
 *
-*
-* Bidirectional search is used to find the shortest path between a source and
-* destination node. It operates by essentially running two simultaneous BFS's,
-* one from each node. When their searchs collide, we have found a path.
 */
 
 function dijkstra() {
