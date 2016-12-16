14858696fd5a59c0c75d8ada928b923412d148b8
Nick Krambousanos <nkrambo@gmail.com>
2016-12-15 20:34:37 -0800

    djikstra

--- assets/graphs/common/shortest_path/dijkstra.js
+++ assets/graphs/common/shortest_path/dijkstra.js
@@ -14,7 +14,7 @@
 * route between two nodes. This is where Dijkstra's algorithm comes in.
 *
 * With weighted edges we can see that our previous route (3 edges, weight 14) is
-* now not the fastest. Our fastest route now is A-> F -> D -> E -> H -> C, which
+* now not the fastest. Our fastest route now is A -> F -> D -> E -> H -> C, which
 * is a total of 4 edges but a weight of 5, almost 3 times faster.
 *
 *         8       6
@@ -28,7 +28,7 @@
 * must be positive, if you're dealing with positive and negative weigths there's
 * an algorithm for that too, Bellman-Ford's algorithm.
 *
-*  Dijkstra's algorithm exists in many variants, but the min-priority queue (with a
+* Dijkstra's algorithm exists in many variants, but the min-priority queue (with a
 * fibonnaci heap) implementation is the fastest known. However, specialized cases
 * (such as bounded/integer weights, directed acyclic graphs etc.) can indeed be
 * improved further as detailed specialized variants.
@@ -41,11 +41,50 @@
 * The algorithm is widely used in network routing protocols and finding the shortest
 * route between cities on road networks, for example.
 *
+* Let's step through how the algorithm works...
+*
+* We have a positively weighted directional graph with 4 nodes. We are starting
+* at node A and want the fastest route to node D. The weights represent minutes
+* to node.
+*
+*    (6)   A   (2)            A -> B: 6 mins
+*        ↙  ↘                 A -> C: 2 mins
+*       B  →  C   (3)         B -> C: 3 mins
+*        ↘   ↙                B -> D: 5 mins
+*    (5)   D   (1)            C -> D: 1 mins
+*
+* Step 1: Find the cheapest node.
+*
+* If we start at node A, should we go to node B or C? Well, if we take B it will
+* cost us 6 minutes but if we take C it will cost us 2 minutes. Therefore, C is
+* the cheapest node. The rest of the nodes we don't know yet.
+*
+* Because we don't know how long it will take to reach node D yet, we mark it as
+* Infinity.
+*
+* Node | Time
+* -------------
+*   B  |  6
+*   C  |  2
+*   D  |  ∞
+*
+* Step 2: Update the costs of the neighbors of the cheapest node we picked.
+*
+* 
+*
+* Step 3: Repeat Step 1 and 2 for every node is the graph.
+*
+* Step 4: Calculate the final path.
+*
 * Time: O(1)
 * Space: O(1)
+*
+* @param {object} graph
+* @param {object} source node to start with
+* @return {void}
 */
 
-function dijkstra() {
+function dijkstra(graph, source) {
 
 }
 
