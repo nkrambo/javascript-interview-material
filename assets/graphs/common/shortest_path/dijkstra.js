d412a2611d5304ffc16ef28cfb218a92a737acf4
Nick Krambousanos <nkrambo@gmail.com>
2016-12-15 17:21:57 -0800

    shortest route

--- assets/graphs/common/shortest_path/dijkstra.js
+++ assets/graphs/common/shortest_path/dijkstra.js
@@ -2,12 +2,36 @@
 /**
 * Dijkstra's Algorithm
 *
-* Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes
-* in directed graphs with unbounded non-negative weights. The algorithm exists in
-* many variants, but the min-priority queue (with a fibonnaci heap) implementation
-* is the fastest known. However, specialized cases (such as bounded/integer weights,
-* directed acyclic graphs etc.) can indeed be improved further as detailed specialized
-* variants.
+* With a BFS you can determine the shortest route between nodes A and C, 3 edges
+* in this case. A -> B -> G -> C
+*
+*    (A) –––→ (B) –––→ (G)
+*     ↑                   ↘
+*     F –→ D –→ E –→ H –→ (C)
+*
+* But what if those edges have travel times associated with them. That is,
+* each edge is weighted. Now the least amount of edges may not be the 'fastest'
+* route between two nodes. This is where Dijkstra's algorithm comes in.
+*
+* With weighted edges we can see that our previous route (3 edges, weight 14) is
+* now not the fastest. Our fastest route now is F -> D -> E -> H -> C, which is a
+* total of 4 edges but a weight of 5, almost 3 times faster.
+*
+*           8           6
+*     A ––––––––→ B ––––––––→ G
+*     ↑                         ↘
+*    (F) –→ (D) –→ (E) –→ (H) –→ (C)
+*        1      1      2      1
+*
+* Dijkstra's is used therefore for finding the shortest paths between nodes
+* in directed graphs with unbounded non-negative weights. Note, that the weights
+* must be positive, if you're dealing with positive and negative weigths there's
+* an algorithm for that too, Bellman-Ford's algorithm.
+*
+*  Dijkstra's algorithm exists in many variants, but the min-priority queue (with a
+* fibonnaci heap) implementation is the fastest known. However, specialized cases
+* (such as bounded/integer weights, directed acyclic graphs etc.) can indeed be
+* improved further as detailed specialized variants.
 *
 * For a given source node in the graph, the algorithm finds the shortest path
 * between that node and every other. It can also be used for finding the shortest
@@ -16,10 +40,6 @@
 *
 * The algorithm is widely used in network routing protocols and finding the shortest
 * route between cities on road networks, for example.
-*
-* Dijkstra's algorithm is used for finding the shortest paths between two nodes
-* in a weighted directed graph where all weights are positive values. The graph
-* might also have cycles.
 *
 * Time: O(1)
 * Space: O(1)
--- assets/graphs/common/shortest_path/shortest_path.md
+++ assets/graphs/common/shortest_path/shortest_path.md
@@ -2,6 +2,5 @@
 ## Shortest Path
 
 * BFS is used to calculate the shortest path for an unweighted graph.
-* Dijkstra's algorithm is used to calculate the shortest path for a weighted graph.
-* Dijkstra's algorithm works when all the weights are positive.
+* Dijkstra's algorithm is used to calculate the shortest path for a weighted graph, when all the weights are positive.
 * If you have negative weights, use the Bellman-Ford algorithm.
