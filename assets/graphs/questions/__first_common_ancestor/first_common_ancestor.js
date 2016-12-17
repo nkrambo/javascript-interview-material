ad454ddadf3c07b545448977838a31b1cdc0edca
Nick Krambousanos <nkrambo@gmail.com>
2016-12-16 22:49:28 -0800

    common ancestor

--- assets/graphs/questions/__first_common_ancestor/first_common_ancestor.js
+++ assets/graphs/questions/__first_common_ancestor/first_common_ancestor.js
@@ -2,25 +2,35 @@
 /**
 * First Common Ancestor
 *
-* Design an algorithm and write code to find the first common ancestor of two nodes in
-* a binary tree. Avoid storing additional nodes in a data structure.
-* NOTE: This is not necessarily a BST
+* Design an algorithm and write code to find the first common ancestor of two
+* nodes in a binary tree. Avoid storing additional nodes in a data structure.
 *
+* This is not necessarily a BST.
 */
 
 /**
-* Common ancestor with parent links
+* With parent links
 *
-* Solution: If each node has a link to their parents we can trace the paths up until they intersect.
-* Time - O(D), where D is the depth of the deeper node
+* Solution:
 *
-* @param {object} p Node
-* @param {object} q Node
-* @return {object} Returns a node that is the first common ancestor of p, q
+* If this were a BST, we could modify the 'find' method for the two nodes and see
+* where the paths diverge. Unfortunately, this is not a BST, so we need a different
+* approach.
+*
+* Time: O(D)
+* Space: O(1)
+*
+* Where D is the depth of the deeper node.
+*
+* @param {object} p binary tree
+* @param {object} q binary tree
+* @return {object} returns a node that is the first common ancestor of p, q
 */
 
-function commonAncestorWithParents(p, q) {
-  const delta = Maths.abs(depth(p) - depth(q)); // difference in depths
+function withParents(p, q) {
+
+  // get difference in depths between nodes
+  const delta = Math.abs(depth(p) - depth(q));
   let first = delta > 0 ? q : p; // shallow
   let second = delta > 0 ? p : q; // deep
 
@@ -31,58 +41,81 @@ function commonAncestorWithParents(p, q) {
     first = first.parent;
     second = second.parent;
   }
+
   return first === null || second === null ? null : first;
 }
+
+/**
+* goUpBy()
+*
+* @param {object} node
+* @param {object} delta the difference in depths
+* @return {object} returns node at delta distance up from node
+*/
 
 function goUpBy(node, delta) {
   while (delta > 0 && node !== null) {
     node = node.parent;
-    delta--;
+    delta -= 1;
   }
+
   return node;
 }
+
+/**
+* depth()
+*
+* @param {object} node
+* @return {number} returns the depth value of a binary tree node
+*/
 
 function depth(node) {
   let depth = 0;
   while (node !== null) {
     node = node.parent;
-    depth++;
+    depth += 1;
   }
+
   return depth;
 }
 
 /**
-* Common ancestor with no links
+* Without parent links
 *
-* Solution: With no links to parents, we take a different approach. We follow a chain in
+* Solution:
+*
+* With no links to parents, we take a different approach. We follow a chain in
 * which p and q are on the same side. That is, if p and q are both on the left side of
 * the node, branch left to look for the common ancestor. If they are both on the right,
 * branch right. When p and q are no longer on the same side, you must have found the
 * common ancestor.
 *
-* @param {object} tree Binary search tree
-* @param {object} p Node
-* @param {object} q Node
-* @return {object} Returns a node that is the first common ancestor of p, q
+* Time: O(1)
+* Space: O(1)
+*
+* @param {object} tree binary tree
+* @param {object} p binary tree node
+* @param {object} q binary tree node
+* @return {object} returns a node that is the first common ancestor of p, q
 */
 
-function commonAncestor(root, p, q) {
+function withoutParents(root, p, q) {
 
-  // check tree root
-  if (root === null) return null;
-
-  // if one of these matches, it is the LCA
-  if (root === p || root === q) return root;
-
-  let left = commonAncestor(root.left, p, q);
-  let right = commonAncestor(root.right, p, q);
-
-  // if p and q are on both sides
-  if (left && right) return root;
-
-  // either one node is on one branch,
-  // or none were found in any branches
-  return left ? left : right;
+  // // check tree root
+  // if (root === null) return null;
+  //
+  // // if one of these matches, it is the LCA
+  // if (root === p || root === q) return root;
+  //
+  // let left = withoutParents(root.left, p, q);
+  // let right = withoutParents(root.right, p, q);
+  //
+  // // if p and q are on both sides
+  // if (left && right) return root;
+  //
+  // // either one node is on one branch,
+  // // or none were found in any branches
+  // return left ? left : right;
 }
 
-export { commonAncestorWithParents, commonAncestor };
+export { withParents, withoutParents };
--- assets/graphs/questions/__first_common_ancestor/test/first_common_ancestor.spec.js
+++ assets/graphs/questions/__first_common_ancestor/test/first_common_ancestor.spec.js
@@ -1,15 +1,100 @@
 
 import { expect } from 'chai';
-import { commonAncestorWithParents, commonAncestor } from '../first_common_ancestor';
+import BinaryTree from '../../../class/binary_tree/binary_tree';
+import { withParents, withoutParents } from '../first_common_ancestor';
 
-describe('commonAncestorWithParents()', () => {
-  // it('should', () => {
-  //
-  // });
+// new class with links to node parents
+class BinaryTreeWP extends BinaryTree {
+  constructor() {
+    super();
+  }
+
+  insert(value) {
+
+    // create node
+    const node = {
+      value,
+      left: null,
+      right: null,
+      parent: null
+    };
+
+    // check root
+    if (this.root === null) {
+      this.root = node;
+      return;
+    }
+
+    const queue = [this.root];
+
+    while (queue.length) {
+
+      // dequeue
+      const current = queue.shift();
+
+      // check to insert
+      if (current.left === null) {
+        node.parent = current;
+        current.left = node;
+        return;
+
+      } else if (current.right === null) {
+        node.parent = current;
+        current.right = node;
+        return;
+      }
+
+      // enqueue
+      if (current.left !== null) {
+        queue.push(current.left);
+      }
+
+      if (current.right !== null) {
+        queue.push(current.right);
+      }
+    }
+  }
+}
+
+/**
+*                1
+*             ↙     ↘
+*          2           3
+*        ↙   ↘       ↙   ↘
+*      4       5   6       7
+*
+*/
+
+describe('withParents()', () => {
+  it('should find the first common ancestor', () => {
+    const BT = new BinaryTreeWP();
+    BT.insert(1);
+    BT.insert(2);
+    BT.insert(3);
+    BT.insert(4);
+    BT.insert(5);
+    BT.insert(6);
+    BT.insert(7);
+
+    const root = BT.root;
+
+    const two = BT.root.left;
+    const three = BT.root.right;
+    expect(withParents(two, three)).to.equal(root);
+
+    const four = BT.root.left.left;
+    const seven = BT.root.right.right;
+    expect(withParents(four, seven)).to.equal(root);
+
+    const five = BT.root.left.right;
+    expect(withParents(five, three)).to.equal(root);
+
+    expect(withParents(five, four)).to.equal(two);
+  });
 });
 
-describe('commonAncestor()', () => {
-  // it('should', () => {
-  //
-  // });
-});
+// describe('commonAncestor()', () => {
+//   // it('should', () => {
+//   //
+//   // });
+// });
