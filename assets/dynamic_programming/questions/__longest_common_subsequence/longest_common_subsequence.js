a0c05242723ba3569261126b91750a18735cbbca
Nick Krambousanos <nkrambo@gmail.com>
2017-01-09 15:11:00 -0800

    lcs

--- assets/dynamic_programming/questions/__longest_common_subsequence/longest_common_subsequence.js
+++ assets/dynamic_programming/questions/__longest_common_subsequence/longest_common_subsequence.js
@@ -5,6 +5,7 @@
 * Tags: Bottom Up, Classic Problems
 *
 * Given two strings s1 and s2, find the longest common subsequence between them.
+* That is, the number of letters in a sequence that the two words have in common.
 *
 * Example:
 * Input: 'abcdaf', 'acbcf'
@@ -39,6 +40,62 @@
 * |  f  |   1     2     3     3     3     4   |
 * +-------------------------------------------+
 *
+* Let's step through our grid...
+*
+* In our first cell we are looking for the longest subsequence between 'a' and 'a'.
+* This is pretty straight forward, since they're the same character, it's 1. We
+* then step through every other column and compare it to the first row 'a' in the
+* same fashion. As in, next we want the longest in 'a' and 'ab', the 'a' and 'abc',
+* and so forth. As you can see the 'best', or longest is 1 across the board. Easy.
+*
+* Onto the next row and now we include 'c'. That is, now we looking for the longest
+* sequence in 'ac' and 'a'. Well this is 1, notice this is also the result from
+* above, which makes sense. Same with the next cell 'ac' and 'ab'. Again this is 1.
+*
+* Next, 'ac' and 'abc'. This is a little more interesting. Now our current s1 and
+* s2 values match, 'c' and 'c'. Manually looking at this example we can easily see
+* that now our maximum subsequence is 2. That is, 'ac'. There is a pattern forming
+* here...
+*
+* If our row and column values match, then we must obviously increase our count
+* by 1 but what are we building the result on top of? Well, we're building it on
+* top of the top-left value. In this case, 1. So we get 1 + 1 = 2;
+*
+*                 'a'
+*         +-----+-----+
+*         | (1) |  1  |   'a' === 'a'
+*         +---- ↖ ----+   result = top-left + 1;
+*     'a' |  1  |  2  |
+*         +-----+-----+
+*
+* When you think about it, this makes a lot of sense. We want the count for 'a'
+* in 'ab' and we're then including the new matched value, hence the plus 1.
+*
+* If the values do not match then, how do we calculate our result for that cell?
+* We take the maximum value between the cells to the left and top.
+*
+*                 'd'
+*         +-----+-----+
+*         |  2  | (2) |   'a' !== 'c'
+*         +------  ↑  +   result = Math.max(left, top);
+*     'c' | (3) ←  3  |
+*         +-----+-----+
+*
+* This also makes sense when we think about it. Look at the grid about at 'c'
+* downward and then 'd' across. This result was calculated using this method. We
+* took 3 from the left as it was greater than 2 above.
+*
+* We continue with this method until we resolve all cells and are left with our
+* final result of 4, in the bottom-right corner.
+*
+* So the formula for this algorithm looks like the following:
+*
+* if (s1[i] === s2[j]) {
+*   T[i][j] = T[i-1][j-1] + 1;
+* } else {
+*   T[i][j] = max(T[i-1][j], T[i][j-1])
+* }
+*
 * Time: O(n^2)
 * Space: O(1)
 *
@@ -49,7 +106,18 @@
 
 function longestCommonSub(s1, s2) {
   let count = 0;
+  for (let i of s1) {
 
+    for (let j of s2) {
+      if (i === j) {
+        count =
+      } else {
+        count = Math.max();
+      }
+    }
+  }
+
+  return count;
 }
 
 export default longestCommonSub;
