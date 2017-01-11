7a158049a54dc169041da0be33d74197c2817e8c
Nick Krambousanos <nkrambo@gmail.com>
2017-01-11 00:25:51 -0800

    coins

--- assets/dynamic_programming/questions/__coin_change_II/coin_change_II.js
+++ assets/dynamic_programming/questions/__coin_change_II/coin_change_II.js
@@ -7,6 +7,8 @@
 * Given a total and coins of certain denominations find number of ways total can
 * be formed from coins assuming infinity supply of coins.
 *
+* Assume that the denominations will always be sorted.
+*
 * Example:
 * Input: 11, [1, 5, 6, 8]
 * Output: 1, [5, 6]
@@ -22,13 +24,15 @@
 * +-----+--------------------------------------------------------------------------+
 * |     |   0     1     2     3     4     5     6     7     8     9     10    11   |
 * +--------------------------------------------------------------------------------+
-* |  1  |   0     0     0     0     0     0     0     0     0     0     0     0    |
+* |     |   ∞     ∞     ∞     ∞     ∞     ∞     ∞     ∞     ∞     ∞     ∞     ∞    |
 * +--------------------------------------------------------------------------------+
-* |  5  |   0     0     0     0     0     0     0     0     0     0     0     0    |
+* |  1  |   0     1     2     3     4     5     6     7     8     9     10    11   |
 * +--------------------------------------------------------------------------------+
-* |  6  |   0     0     0     0     0     0     0     0     0     0     0     0    |
+* |  5  |   0     1     2     3     4     1     2     3     4     5     2     3    |
 * +--------------------------------------------------------------------------------+
-* |  8  |   0     0     0     0     0     0     0     0     0     0     0     0    |
+* |  6  |   0     1     2     3     4     1     1     2     3     4     2     2    |
+* +--------------------------------------------------------------------------------+
+* |  8  |   0     1     2     3     4     1     1     2     1     2     2     2    |
 * +--------------------------------------------------------------------------------+
 *
 * Let's step through our grid.
@@ -36,10 +40,10 @@
 *
 * The formula for this is easy:
 *
-*     if (j >= denominations[i]) {
+*     if (j < denominations[i]) {
 *       T[i][j] = T[i - 1][j]
 *     } else {
-*       T[i][j] = T[i - 1][j] + T[i][j - denominations[i]]
+*       T[i][j] = min(T[i - 1][j], T[i][j - denominations[i]] + 1)
 *     }
 *
 * Time: O(n * k)
@@ -53,23 +57,31 @@
 */
 
 function coinMinimum(total, denominations) {
-  const cols = total + 1; // 1 for value 0 in total
-  const rows = denominations.length;
-}
+  const cols = total + 1; // add 1 for 0 col
+  const rows = denominations.length + 1; // add 1 for Infinity row
 
-// cols = total + 1   # 1 for value 0 in total
-// rows = len(coins)
-// T = [[1 if col == 0 else 0 for col in range(cols)] for _ in range(rows)]
-//
-// for i in range(rows):
-//     for j in range(cols):
-//         if (i - 1) < 0:
-//             continue
-//         if j < coins[i]:
-//             T[i][j] = T[i - 1][j]
-//         else:
-//             T[i][j] = T[i - 1][j] + T[i][j - coins[i]]
-//
-// return T[rows - 1][cols - 1]
+  // build matrix, 0s by default
+  const matrix = [];
+  for (let i = 0; i < rows; i += 1) {
+    matrix[i] = new Array(cols).fill(0);
+  }
+
+  // add Infinity row
+  matrix[0].fill(Infinity);
+
+  for (let i = 1; i < rows; i += 1) {
+    for (let j = 1; j < cols; j += 1) {
+
+      if (j < denominations[i-1]) {
+        matrix[i][j] = matrix[i-1][j];
+
+      } else {
+        matrix[i][j] = Math.min(matrix[i-1][j], matrix[i][j-denominations[i-1]] + 1);
+      }
+    }
+  }
+
+  return matrix[rows-1][cols-1];
+}
 
 export default coinMinimum;
