6d904a4177b75e90ce7226845675a1ed0ab177b8
Nick Krambousanos <nkrambo@gmail.com>
2017-01-11 01:58:24 -0800

    coin

--- assets/dynamic_programming/questions/__coin_change_II/coin_change_II.js
+++ assets/dynamic_programming/questions/__coin_change_II/coin_change_II.js
@@ -19,7 +19,11 @@
 *
 * Solution:
 *
+* We solve this with dynamic programming, so we'll start with a matrix.
+*
 * Across the top we have 0 to 'total' and down the side we have the coin 'denominations'.
+* We also have a 0s column and an Infinity row, these are important for our final
+* formula to work.
 *
 * +-----+--------------------------------------------------------------------------+
 * |     |   0     1     2     3     4     5     6     7     8     9     10    11   |
@@ -35,8 +39,37 @@
 * |  8  |   0     1     2     3     4     1     1     2     1     2     2     2    |
 * +--------------------------------------------------------------------------------+
 *
-* Let's step through our grid.
+* Let's step through our matrix.
 *
+* Well firstly, we know that if we have a 0 total then the minimum coins will also
+* be 0 across the board.
+*
+* Next, denomination of 1. For a total of 1, we will need exactly 1, for 2 we need
+* 2 and so on, all the way to 11. In other words, we're saying how many coins do
+* we need to total 11 cents, if we only had 1c coints... 11!
+*
+* Next, denomination of 5. As we step across we can see that we cannot make a total
+* of 1c from a 5c coin, so we must take the result above as this is the best we
+* can do for this total. This is the same until we get to a totol of 5. Obviously,
+* a 5c coin totals 5c exacly once, so the answer is 1.
+*
+* But how do we calculate this result instead? Well, we take the minimum between
+* the top result, which is the best so far, and our (current position - denomination) + 1.
+*
+* If you think about it, this makes sense. Because we have a 0s column, we know
+* that when we hit a total equal to the denomination, if we step back to 0 + 1,
+* for the current coin, we have a result that is correct.
+*
+* Same for the next cell too, 6. We count our current coin '5' and take the best
+* result from the 1 denomination, if this is better than what we have for 6 then
+* we take it. (1 + 1) is lower than 6, so we now have 2.
+*
+* Again, notice that we have a row of Infinity and a 0s column. We basically need
+* these to get the results to start resolving without an error, especially in the
+* first row, where our formula requires us to step up a row for best results.
+*
+* When we step through and resolve our whole matrix like this we get the final
+* result at the bottom right.
 *
 * The formula for this is easy:
 *
@@ -57,7 +90,7 @@
 */
 
 function coinMinimum(total, denominations) {
-  const cols = total + 1; // add 1 for 0 col
+  const cols = total + 1;                // add 1 for 0 col
   const rows = denominations.length + 1; // add 1 for Infinity row
 
   // build matrix, 0s by default
@@ -69,6 +102,7 @@ function coinMinimum(total, denominations) {
   // add Infinity row
   matrix[0].fill(Infinity);
 
+  // solve sub-problems
   for (let i = 1; i < rows; i += 1) {
     for (let j = 1; j < cols; j += 1) {
 
--- assets/dynamic_programming/questions/__coin_change_II/test/coin_change_II.spec.js
+++ assets/dynamic_programming/questions/__coin_change_II/test/coin_change_II.spec.js
@@ -9,10 +9,10 @@ describe('coinMinimum()', () => {
     // 5, 6
     expect(coinMinimum(11, [1, 5, 6, 8])).to.equal(2);
 
-    // // 6
-    // expect(coinMinimum(6, [1, 5, 6, 8])).to.equal(1);
-    //
-    // // 1, 1, 1, 1
-    // expect(coinMinimum(4, [1, 5, 6, 8])).to.equal(4);
+    // 6
+    expect(coinMinimum(6, [1, 5, 6, 8])).to.equal(1);
+
+    // 1, 1, 1, 1
+    expect(coinMinimum(4, [1, 5, 6, 8])).to.equal(4);
   });
 });
