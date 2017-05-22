e81a026797c91facae71e77ee686b9700a29d6fc
Nick Krambousanos <nkrambo@gmail.com>
2017-05-22 11:43:20 -0700

    Longest sub string no repeats

--- assets/strings/questions/longest_substring_no_repeats/longest_substring_no_repeats.js
+++ assets/strings/questions/longest_substring_no_repeats/longest_substring_no_repeats.js
@@ -28,29 +28,76 @@
 */
 
 /**
-* longestSubstring()
+* longestSubstringBrute()
 *
 * Solution:
 *
-* Time: O(1)
-* Space: O(1)
+* We can brute force this one by check all the substrings one by one to see if it
+* has no duplicate character.
+*
+* Suppose we have a helper function allUnique(s) which will return true if the
+* characters in the substring are all unique, otherwise false. We can iterate through
+* all the possible substrings of the given string s and call the function allUnique.
+*
+* If it turns out to be true, then we update our answer of the maximum length of
+* substring without duplicate characters.
+*
+* Now let's fill the missing parts:
+*
+* To enumerate all substrings of a given string, we enumerate the start and end
+* indices of them. Suppose the start and end indices are i and j, respectively.
+* Then we have 0 ≤ i < j ≤ n (here end index j is exclusive by convention). Thus,
+* using two nested loops with i from 0 to n - 1 and j from i+1 to n, we can
+* enumerate all the substrings of s.
+*
+* To check if one string has duplicate characters, we can use a set. We iterate
+* through all the characters in the string and put them into the set one by one.
+* Before putting one character, we check if the set already contains it. If so,
+* we return false. After the loop, we return true.
+*
+* Time: O(n^3)
+* Space: O(k)
+*
+* Where k is the size of the Set. The size of the Set is upper bounded by the size
+* of the string n and the size of the charset/alphabet m.
 *
 * @param {string} s string of (n) length
 * @return {number} returns length of the longest substring with no repeated characters
 */
 
-function longestSubstring(s) {
-  
-  // init a result count
-  // init a map for chars seen
+function longestSubstringBrute(s) {
+  let max = 0;
 
-  // step over string
+  for (let i = 0; i < s.length; i += 1) {
+    for (let j = i + 1; j <= s.length; j += 1) {
+      if (allUnique(s, i, j)) {
+        max = Math.max(max, j - i)
+      }
+    }
+  }
 
-  // check map for char
-    // if not seen: add char to map, increase result count, keep going
-
-    // if seen:
-
+  return max;
 }
 
-export default longestSubstring;
+/**
+* allUnique()
+*
+* @param {string} s string of (n) length
+* @param {string} start index of s
+* @param {string} end index of s
+* @return {boolean} returns true if all the characters in s are unique, otherwise false
+*/
+
+function allUnique(s, start, end) {
+  const seen = new Set();
+
+  for (let i = start; i < end; i += 1) {
+    if (seen.has(s[i])) return false;
+    seen.add(s[i]);
+  }
+
+  return true;
+}
+
+
+export { longestSubstringBrute, allUnique };
--- assets/strings/questions/longest_substring_no_repeats/test/longest_substring_no_repeats.spec.js
+++ assets/strings/questions/longest_substring_no_repeats/test/longest_substring_no_repeats.spec.js
@@ -1,11 +1,39 @@
 
 import { expect } from 'chai';
-import longestSubstring from '../longest_substring_no_repeats';
+import { longestSubstringBrute, allUnique } from '../longest_substring_no_repeats';
 
-describe('longestSubstring()', () => {
+describe('longestSubstringBrute()', () => {
   it('should find the longest substring with no repeating characters', () => {
-    expect(longestSubstring('abcabcbb')).to.equal(3);
-    expect(longestSubstring('bbbbb')).to.equal(1);
-    expect(longestSubstring('pwwkew')).to.equal(3);
+    expect(longestSubstringBrute('abcabcbb')).to.equal(3);
+    expect(longestSubstringBrute('bbbbb')).to.equal(1);
+    expect(longestSubstringBrute('pwwkew')).to.equal(3);
+    expect(longestSubstringBrute('c')).to.equal(1);
+    expect(longestSubstringBrute('au')).to.equal(2);
+    expect(longestSubstringBrute('dvdf')).to.equal(3);
   });
 });
+
+describe('allUnique()', () => {
+  it('should return true if all characters of a string are unique', () => {
+    expect(allUnique('abc', 0, 2)).to.be.true;
+    expect(allUnique('a', 0, 0)).to.be.true;
+    expect(allUnique('ab', 0, 1)).to.be.true;
+  });
+
+  it('should return false if all characters of a string are NOT unique', () => {
+    expect(allUnique('aa', 0, 2)).to.be.false;
+    expect(allUnique('abca', 0, 4)).to.be.false;
+    expect(allUnique('aaaaa', 0, 5)).to.be.false;
+  });
+});
+
+// describe('longestSubstringAlt()', () => {
+//   it('should find the longest substring with no repeating characters', () => {
+//     expect(longestSubstringAlt('abcabcbb')).to.equal(3);
+//     expect(longestSubstringAlt('bbbbb')).to.equal(1);
+//     expect(longestSubstringAlt('pwwkew')).to.equal(3);
+//     expect(longestSubstringAlt('c')).to.equal(1);
+//     expect(longestSubstringAlt('au')).to.equal(2);
+//     expect(longestSubstringAlt('dvdf')).to.equal(3);
+//   });
+// });
