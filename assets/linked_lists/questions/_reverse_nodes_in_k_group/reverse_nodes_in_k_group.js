df2af669ab94adf6f95164d026ddaf6045039e05
Nick Krambousanos <nkrambo@gmail.com>
2017-05-31 09:28:21 +1000

    Stub leet

---
+++ assets/linked_lists/questions/_reverse_nodes_in_k_group/reverse_nodes_in_k_group.js
@@ -0,0 +1,57 @@
+
+/**
+* Reverse Nodes in k Group
+*
+* Tags: Linked List
+* Leetcode: 25
+*
+* Given a linked list, reverse the nodes of a linked list k at a time and return
+* its modified list.
+*
+* k is a positive integer and is less than or equal to the length of the linked
+* list. If the number of nodes is not a multiple of k then left-out nodes in the
+* end should remain as it is.
+*
+* You may not alter the values in the nodes, only nodes itself may be changed.
+*
+* Only constant memory is allowed.
+*
+* Example 1:
+*
+* Given 1 → 2 → 3 → 4 → 5, k = 2
+* Return 2 → 1 → 4 → 3 → 5
+*
+* Example 2:
+*
+* Given 1 → 2 → 3 → 4 → 5, k = 3
+* Return 3 → 2 → 1 → 4 → 5
+*/
+
+/**
+* Definition for singly-linked list.
+*
+* function ListNode(val) {
+*   this.val = val;
+*   this.next = null;
+* }
+*/
+
+/**
+* reverseKGroup()
+*
+* Solution:
+*
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {object} head linked list node
+* @param {number} k number to reverse nodes at a time
+* @return {boolean} returns
+*/
+
+// function reverseKGroup(head, k) {
+//
+// }
+//
+// export default reverseKGroup;
---
+++ assets/linked_lists/questions/_reverse_nodes_in_k_group/test/reverse_nodes_in_k_group.spec.js
@@ -0,0 +1,9 @@
+//
+// import { expect } from 'chai';
+// import reverseKGroup from '../reverse_nodes_in_k_group';
+//
+// describe('reverseKGroup()', () => {
+//   it('should return a linked list with nodes reversed k times', () => {
+//
+//   });
+// });
