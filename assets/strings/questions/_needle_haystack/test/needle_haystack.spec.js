8839ece5dec29967399eef235a12dfd42c546355
Nick Krambousanos <nkrambo@gmail.com>
2017-05-31 13:38:33 +1000

    Remove old needle and haystack

--- assets/strings/questions/_needle_haystack/test/needle_haystack.spec.js
+++
@@ -1,9 +0,0 @@
-//
-// import { expect } from 'chai';
-// import findNeedle from '../needle_haystack';
-//
-// describe('findNeedle()', () => {
-//   it('should return the correct change', () => {
-//
-//   });
-// });
