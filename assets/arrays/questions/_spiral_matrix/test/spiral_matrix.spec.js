5286a7e7ddd68867d9906e4551f66837a75d5b89
Nick Krambousanos <nkrambo@gmail.com>
2017-06-22 22:40:19 +1000

    Update spiral matrix

--- assets/arrays/questions/_spiral_matrix/test/spiral_matrix.spec.js
+++ assets/arrays/questions/_spiral_matrix/test/spiral_matrix.spec.js
@@ -1,6 +1,6 @@
 //
 // import { expect } from 'chai';
-// import spiralOrder from '../spiral_order';
+// import spiralOrder from '../spiral_matrix';
 //
 // describe('spiralOrder()', () => {
 //   it('should return the elements of an m x n matrix in spiral order', () => {
