47d5990ab741d26a09dff92b43854c1d6a5748d4
Nick Krambousanos <nkrambo@gmail.com>
2017-07-14 23:52:36 +1000

    Stub

---
+++ assets/graphs/questions/binary_tree/_path_sum/path_sum.js
@@ -0,0 +1,37 @@
+
+/**
+* Path Sum
+*
+* Tags: Tree, DFS
+* Leetcode: 112
+*
+* Given a binary tree and a sum, determine if the tree has a root-to-leaf path
+* such that adding up all the values along the path equals the given sum.
+*
+* Example:
+*
+* Given the below binary tree and sum = 22,
+*
+* Return true, as there exist a root-to-leaf path 5 → 4 → 11 → 2 which sum is 22.
+*/
+
+/**
+* Definition for a binary tree node.
+*
+* function TreeNode(val) {
+*   this.val = val;
+*   this.left = this.right = null;
+* }
+*/
+
+/**
+* @param {TreeNode} root
+* @param {number} sum
+* @return {boolean}
+*/
+
+// function hasPathSum(root, sum) {
+//
+// };
+//
+// export default hasPathSum;
