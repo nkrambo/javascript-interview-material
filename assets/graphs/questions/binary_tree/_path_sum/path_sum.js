6c53ec3fcd6f3759b669905ed3d2c4bd69cdaaaa
Nick Krambousanos <nkrambo@gmail.com>
2017-10-01 00:04:17 -0700

    Stub out path sum

--- assets/graphs/questions/binary_tree/_path_sum/path_sum.js
+++ assets/graphs/questions/binary_tree/_path_sum/path_sum.js
@@ -12,6 +12,14 @@
 *
 * Given the below binary tree and sum = 22,
 *
+*                  5
+*               ↙     ↘
+*             4         8
+*           ↙         ↙   ↘
+*         11        13      4
+*       ↙    ↘                ↘
+*     7        2                1
+*
 * Return true, as there exist a root-to-leaf path 5 → 4 → 11 → 2 which sum is 22.
 */
 
