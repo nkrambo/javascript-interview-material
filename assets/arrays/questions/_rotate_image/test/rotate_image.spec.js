41fce766aede64ed80008d17343e49ad46df5ae2
Nick Krambousanos <nkrambo@gmail.com>
2017-10-08 12:43:36 -0700

    Test

--- assets/arrays/questions/_rotate_image/test/rotate_image.spec.js
+++ assets/arrays/questions/_rotate_image/test/rotate_image.spec.js
@@ -1,24 +1,23 @@
-//
-// import { expect } from 'chai';
-// import rotate from '../rotate_image';
-//
-// describe('rotate()', () => {
-//   it('should return a rotated matrix 90CW', () => {
-//     const matrix = [[1, 1, 1, 1],
-//                     [2, 2, 2, 2],
-//                     [3, 3, 3, 3],
-//                     [4, 4, 4, 4]];
-//
-//     const rotated = [[4, 3, 2, 1],
-//                      [4, 3, 2, 1],
-//                      [4, 3, 2, 1],
-//                      [4, 3, 2, 1]];
-//
-//     expect(rotate(matrix)).to.deep.equal(rotated);
-//   });
-//
-//   it('should return an unchanged 1, 1 matrix', () => {
-//     const smallMatrix = [[0]];
-//     expect(rotate(smallMatrix)).to.equal(smallMatrix);
-//   });
-// });
+
+import rotate from '../rotate_image';
+
+describe.skip('rotate()', () => {
+  it('should return a rotated matrix 90CW', () => {
+    const matrix = [[1, 1, 1, 1],
+                    [2, 2, 2, 2],
+                    [3, 3, 3, 3],
+                    [4, 4, 4, 4]];
+
+    const rotated = [[4, 3, 2, 1],
+                     [4, 3, 2, 1],
+                     [4, 3, 2, 1],
+                     [4, 3, 2, 1]];
+
+    expect(rotate(matrix)).toEqual(rotated);
+  });
+
+  it('should return an unchanged 1, 1 matrix', () => {
+    const smallMatrix = [[0]];
+    expect(rotate(smallMatrix)).toBe(smallMatrix);
+  });
+});
--- assets/sort/algorithms/_heap_sort/test/heap_sort.spec.js
+++ assets/sort/algorithms/_heap_sort/test/heap_sort.spec.js
@@ -1,10 +1,9 @@
-//
-// import { expect } from 'chai';
-// import heapSort from '../heap_sort';
-//
-// describe('heapSort()', () => {
-//   it('should return a new sorted array', () => {
-//     expect(heapSort([4, 3, 7, 1, 2, 5, 9])).to.deep.equal([1, 2, 3, 4, 5, 7, 9]);
-//     expect(heapSort([3, 5, 3, 3, 5])).to.deep.equal([3, 3, 3, 5, 5]);
-//   });
-// });
+
+import heapSort from '../heap_sort';
+
+describe.skip('heapSort()', () => {
+  it('should return a new sorted array', () => {
+    expect(heapSort([4, 3, 7, 1, 2, 5, 9])).toEqual([1, 2, 3, 4, 5, 7, 9]);
+    expect(heapSort([3, 5, 3, 3, 5])).toEqual([3, 3, 3, 5, 5]);
+  });
+});
