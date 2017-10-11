0d5e43883ff7d236a3721f60c244e4cc1cf7dfaf
Nick Krambousanos <nkrambo@gmail.com>
2017-10-10 20:46:19 -0700

    Update test

--- assets/linked_lists/questions/_delete_node_in_a_linked_list/test/delete_node_in_a_linked_list.spec.js
+++ assets/linked_lists/questions/_delete_node_in_a_linked_list/test/delete_node_in_a_linked_list.spec.js
@@ -1,9 +1,8 @@
-//
-// import { expect } from 'chai';
+
 // import deleteNode from '../delete_node_in_a_linked_list';
-//
-// describe('deleteNode()', () => {
-//   it('should', () => {
-//
-//   });
-// });
+
+describe.skip('deleteNode()', () => {
+  it('should', () => {
+
+  });
+});
