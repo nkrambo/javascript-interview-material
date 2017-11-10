1f700ed5ad1087adc4017e01fbeb5fd62b40593d
Nick Krambousanos <nkrambo@gmail.com>
2017-11-10 14:26:38 -0800

    find square root

--- assets/search/questions/_sqrt(x)/sqrt.js
+++ assets/search/questions/_sqrt(x)/sqrt.js
@@ -15,7 +15,48 @@
 *
 * Solution:
 *
-* https://leetcode.com/problems/sqrtx/#/solutions
+* This is one of those problems that can be solved in many ways.
+*
+* The 3 most popular methods are using:
+*
+* 1) Newton's method
+* 2) Binary search
+* 3) Bit manipulation
+*
+* We can use Newton's method for finding successively better approximations to the
+* roots (or zeroes) of a real-valued function. It is one example of a root-finding
+* algorithm.
+*
+* The idea of the method is as follows: one starts with an initial guess which is
+* reasonably close to the true root, then the function is approximated by its tangent
+* line and then computing the x-intercept of this tangent line. This x-intercept
+* will typically be a better approximation to the function's root than the original
+* guess, and the method can be iterated.
+*
+* Time: O(1)
+* Space: O(1)
+*
+* https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division
+* https://en.wikipedia.org/wiki/Newton%27s_method
+*
+* @param {number} x number
+* @return {number} returns the square root of x
+*/
+
+function mySqrt(x) {
+  let r = x;
+  while (r * r > x) {
+    r = ((r + (x / r)) / 2) | 0;
+  }
+
+  return r;
+}
+
+/**
+* mySqrtBit()
+*
+* Solution:
+*
 *
 * Time: O(1)
 * Space: O(1)
@@ -24,8 +65,40 @@
 * @return {number} returns the square root of x
 */
 
-// function mySqrt(x) {
-//
-// }
-//
-// export default mySqrt;
+function mySqrtBit(x) {
+
+}
+
+/**
+* mySqrtBS()
+*
+* Solution:
+*
+*
+* Time: O(1)
+* Space: O(1)
+*
+* @param {number} x number
+* @return {number} returns the square root of x
+*/
+
+function mySqrtBS(x) {
+  // catch edge
+  if (x === 0) return 0;
+
+  let left = 1;
+  let right = Number.MAX_SAFE_INTEGER;
+
+  while (true) { // eslint-disable-line no-constant-condition
+    const mid = Math.floor((left + right) / 2);
+
+    if (mid > x / mid) {
+      right = mid - 1;
+    } else {
+      if (mid + 1 > x / (mid + 1)) return mid;
+      left = mid + 1;
+    }
+  }
+}
+
+export { mySqrt, mySqrtBit, mySqrtBS };
--- assets/search/questions/_sqrt(x)/test/sqrt.spec.js
+++ assets/search/questions/_sqrt(x)/test/sqrt.spec.js
@@ -1,8 +1,29 @@
 
-// import mySqrt from '../sqrt';
+import { mySqrt, mySqrtBit, mySqrtBS } from '../sqrt';
 
-describe.skip('mySqrt()', () => {
+describe('mySqrt()', () => {
   test('should return the square root', () => {
+    expect(mySqrt(1)).toBe(1);
+    expect(mySqrt(4)).toBe(2);
+    expect(mySqrt(8)).toBe(2);
+    expect(mySqrt(64)).toBe(8);
+  });
+});
 
+describe.skip('mySqrtBit()', () => {
+  test('should return the square root', () => {
+    expect(mySqrtBit(1)).toBe(1);
+    expect(mySqrtBit(4)).toBe(2);
+    expect(mySqrtBit(8)).toBe(2);
+    expect(mySqrtBit(64)).toBe(8);
+  });
+});
+
+describe('mySqrtBS()', () => {
+  test('should return the square root', () => {
+    expect(mySqrtBS(1)).toBe(1);
+    expect(mySqrtBS(4)).toBe(2);
+    expect(mySqrtBS(8)).toBe(2);
+    expect(mySqrtBS(64)).toBe(8);
   });
 });
