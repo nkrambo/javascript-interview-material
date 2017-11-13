d179c7ccb90481bc5f93eb2bcf7175974a9b4b9c
Nick Krambousanos <nkrambo@gmail.com>
2017-11-13 10:26:38 -0800

    cache

--- assets/design/questions/_LRU_cache/test/LRU_cache.spec.js
+++ assets/design/questions/_LRU_cache/test/LRU_cache.spec.js
@@ -7,4 +7,16 @@ describe('LRUCache()', () => {
 
     expect(cache.capacity).toBe(1000);
   });
+
+  test('should PUT a new cache item', () => {
+    const cache = new LRUCache(1000);
+
+    expect(cache.capacity).toBe(1000);
+  });
+
+  test('should GET a cache item', () => {
+    const cache = new LRUCache(1000);
+
+    expect(cache.capacity).toBe(1000);
+  });
 });
