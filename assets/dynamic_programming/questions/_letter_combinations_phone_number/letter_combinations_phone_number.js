caf3e84911f9c1886a26287ddba84d5e570fd1ef
Nick Krambousanos <nkrambo@gmail.com>
2018-02-11 17:10:41 -0800

    Add in DOM

--- assets/dynamic_programming/questions/_letter_combinations_phone_number/letter_combinations_phone_number.js
+++ assets/dynamic_programming/questions/_letter_combinations_phone_number/letter_combinations_phone_number.js
@@ -97,4 +97,76 @@ function letterCombinations(digits) {
   return queue;
 }
 
-export default letterCombinations;
+function letterCombinationsBT(digits) {
+  const combos = [];
+
+  // catch edge
+  if (!digits.length) return combos;
+
+  let local = '';
+
+  // map our digits to strings
+  const buttons = new Map();
+  buttons.set('2', 'abc');
+  buttons.set('3', 'def');
+  buttons.set('4', 'ghi');
+  buttons.set('5', 'jkl');
+  buttons.set('6', 'mno');
+  buttons.set('7', 'pqrs');
+  buttons.set('8', 'tuv');
+  buttons.set('9', 'wxyz');
+
+  backtrack(buttons, combos, local, 0, digits);
+
+  return combos;
+}
+
+function backtrack(buttons, combos, local, index, digits) {
+  // base case
+  if (index === digits.length) {
+    combos.push(local);
+  } else {
+    for (let i = 0; i < buttons.get(); i += 1) {
+      local.concat(buttons); // add character to end
+      backtrack(buttons, combos, local, index + 1, digits); // recurse and bump index
+      local = local.substring(0, local.length - 1); // remove last character
+    }
+  }
+
+  //         if(index==digits.size())
+  //             res.push_back(local);
+  //         else
+  //             for(int i=0;i<table[digits[index]-'0'].size();i++) {
+  //                 local.push_back(table[digits[index]-'0'][i]);
+  //                 backtracking(table, res, local, index+1, digits);
+  //                 local.pop_back();
+  //             }
+}
+
+// class Solution {
+// public:
+//     vector<string> letterCombinations(string digits)
+//     {
+//         vector<string> res;
+//         if(digits.size()==0) return res;
+//         string local;
+//         vector<vector<char>> table(2,vector<char>());
+//         table.push_back(vector<char>{'a','b','c'}); // index 2
+//         table.push_back(vector<char>{'d','e','f'}); // 3
+//         table.push_back(vector<char>{'g','h','i'});
+//         table.push_back(vector<char>{'j','k','l'}); // 5
+//         table.push_back(vector<char>{'m','n','o'});
+//         table.push_back(vector<char>{'p','q','r','s'}); // 7
+//         table.push_back(vector<char>{'t','u','v'});
+//         table.push_back(vector<char>{'w','x','y','z'}); // 9
+//
+//         backtracking(table,res,local,0,digits);
+//         return res;
+//     }
+//
+//     void backtracking(const vector<vector<char>>& table, vector<string>& res, string& local, int index, const string& digits) {
+
+//     }
+// };
+
+export { letterCombinations, letterCombinationsBT };
---
+++ assets/misc/symmetric_node/symmetric_node.js
@@ -0,0 +1,90 @@
+
+/**
+* Symmetric Node
+*
+* Given 2 identical DOM trees (but not equal) and one element of the first DOM tree,
+* how would you find this element in the second DOM tree?
+*
+* // DOM Tree 1
+*
+* <div id="root1">
+*   <div>
+*     <div></div>
+*   </div>
+*   <div>
+*     <div id="node1"></div>
+*     <div></div>
+*   </div>
+* </div>
+*
+* // DOM Tree 2
+*
+* <div id="root2">
+*   <div>
+*     <div></div>
+*   </div>
+*   <div>
+*     <div id="node2"></div>
+*     <div></div>
+*   </div>
+* </div>
+*
+*
+*/
+
+// This function returns a real array of Nodes, so we can use methonds like "indexOf".
+// childnodes is a "collection".
+function getChildren(node) {
+  return Array.from(node.childNodes);
+}
+
+// This function returns an array of indices from given node to the root
+function getPath(root, node) {
+  const path = [];
+  let curElement = node;
+
+  // This is important as if a node is null or doesn't have a parent
+  // there is no need of searching further
+  while (curElement !== root && curElement && curElement.parentNode) {
+    const index = getChildren(curElement.parentNode).indexOf(curElement);
+    path.push(index);
+    curElement = curElement.parentNode;
+  }
+
+  return path;
+}
+
+// Popping all values from the array of indices we go to the symmetrical node
+function getNodeByPath(root, originalPath) {
+  const path = [].concat(originalPath);
+  let element = root;
+
+  while (path.length) {
+    element = getChildren(element)[path.pop()];
+  }
+
+  return element;
+}
+
+// For convenience
+function getSymmetricNode(root1, root2, node) {
+  const path = getPath(root1, node);
+  return getNodeByPath(root2, path);
+}
+
+const root1 = document.getElementById('root1');
+const root2 = document.getElementById('root2');
+const node1 = document.getElementById('node1');
+const node2 = document.getElementById('node2');
+
+const nodeX = getSymmetricNode(root1, root2, node1);
+
+console.log(nodeX === node2); // true
+
+
+
+// https://stackoverflow.com/questions/19779438/dom-tree-traversal
+
+// https://javascript.info/dom-navigation
+
+// https://www.gitbook.com/book/timurcatakli/technical-interview-preparation-v2/details
